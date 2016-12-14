083d0948dabbe5c0f5441105f70cff3c49164b3c
Henrik Nårstad <henrik.narstad@knowit.no>
2016-12-14 16:40:36 +0100

    #139 add license and iso639 mappings

---
+++ src/main/scala/no/ndla/mapping/License.scala
@@ -0,0 +1,34 @@
+/*
+ * Part of NDLA mapping.
+ * Copyright (C) 2016 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.mapping
+
+object License {
+  private val licenseToLicenseDefinitionsSeq = Seq(
+    LicenseDefinition("by", "Creative Commons Attribution 2.0 Generic", Some("https://creativecommons.org/licenses/by/2.0/")),
+    LicenseDefinition("by-sa", "Creative Commons Attribution-ShareAlike 2.0 Generic", Some("https://creativecommons.org/licenses/by-sa/2.0/")),
+    LicenseDefinition("by-nc", "Creative Commons Attribution-NonCommercial 2.0 Generic", Some("https://creativecommons.org/licenses/by-nc/2.0/")),
+    LicenseDefinition("by-nd", "Creative Commons Attribution-NoDerivs 2.0 Generic", Some("https://creativecommons.org/licenses/by-nd/2.0/")),
+    LicenseDefinition("by-nc-sa", "Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Generic", Some("https://creativecommons.org/licenses/by-nc-sa/2.0/")),
+    LicenseDefinition("by-nc-nd", "Creative Commons Attribution-NonCommercial-NoDerivs 2.0 Generic", Some("https://creativecommons.org/licenses/by-nc-nd/2.0/")),
+    LicenseDefinition("publicdomain", "Public Domain", Some("https://creativecommons.org/about/pdm")),
+    LicenseDefinition("gnu", "GNU General Public License, version 2", Some("http://www.gnu.org/licenses/old-licenses/gpl-2.0.html")),
+    LicenseDefinition("nolaw", "Public Domain Dedication", Some("http://opendatacommons.org/licenses/pddl/")),
+    LicenseDefinition("nlod", "Norsk lisens for offentlige data", Some("http://data.norge.no/nlod/no/1.0")),
+    LicenseDefinition("noc", "Public Domain Mark", Some("https://creativecommons.org/about/pdm")),
+    LicenseDefinition("copyrighted", "Copyrighted", None)
+  )
+
+  private val licenseToLicenseDefinitionsMap = licenseToLicenseDefinitionsSeq.map(x => x.license -> x).toMap
+
+  def getLicense(code: String): Option[LicenseDefinition] = licenseToLicenseDefinitionsMap.get(code)
+
+  def getLicenses: Seq[LicenseDefinition] = licenseToLicenseDefinitionsSeq
+}
+
+case class LicenseDefinition(license: String, description: String, url: Option[String])
