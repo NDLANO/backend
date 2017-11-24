bc3a2e8880b9b6ed220206ba4b983be6ef51db57
Jonas Natten <jonas.natten@knowit.no>
2017-11-24 14:36:50 +0100

    #784: Adds retrying database connection if PSQL exception is caught

--- src/main/scala/no/ndla/audioapi/model/api/Error.scala
+++ src/main/scala/no/ndla/audioapi/model/api/Error.scala
@@ -30,11 +30,13 @@ object Error {
   val FILE_TOO_BIG = "FILE TOO BIG"
   val ACCESS_DENIED = "ACCESS DENIED"
   val WINDOW_TOO_LARGE = "RESULT_WINDOW_TOO_LARGE"
+  val DATABASE_UNAVAILABLE = "DATABASE_UNAVAILABLE"
 
   val RESOURCE_OUTDATED_DESCRIPTION = "The resource is outdated. Please try fetching before submitting again."
   val GENERIC_DESCRIPTION = s"Ooops. Something we didn't anticipate occured. We have logged the error, and will look into it. But feel free to contact ${AudioApiProperties.ContactEmail} if the error persists."
   val FileTooBigError = Error(FILE_TOO_BIG, s"The file is too big. Max file size is ${AudioApiProperties.MaxAudioFileSizeBytes / 1024 / 1024} MiB")
   val WINDOW_TOO_LARGE_DESCRIPTION = s"The result window is too large. Fetching pages above ${AudioApiProperties.ElasticSearchIndexMaxResultWindow} results are unsupported."
+  val DATABASE_UNAVAILABLE_DESCRIPTION = s"Database seems to be unavailable, retrying connection."
 }
 
 class NotFoundException(message: String = "The audio was not found") extends RuntimeException(message)
