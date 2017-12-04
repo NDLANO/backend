03bb94a9a65140e3ea381dfd37c8b7293b463067
Jonas Natten <jonas.natten@knowit.no>
2017-12-04 10:35:23 +0100

    #784: Improves healthcheck test maintainability

--- src/test/scala/no/ndla/audioapi/controller/HealthControllerTest.scala
+++ src/test/scala/no/ndla/audioapi/controller/HealthControllerTest.scala
@@ -8,78 +8,64 @@
 
 package no.ndla.audioapi.controller
 
+import no.ndla.audioapi.model.api._
 import no.ndla.audioapi.{TestEnvironment, UnitSuite}
 import org.scalatra.test.scalatest.ScalatraFunSuite
 import org.mockito.Mockito._
+import org.json4s.native.Serialization.write
+
 import scalaj.http.HttpResponse
 
 class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraFunSuite {
+  implicit val formats = org.json4s.DefaultFormats
 
-  val audioSearchBody = s"""{
-                           |	"totalCount": 1,
-                           |	"page": 1,
-                           |	"pageSize": 10,
-                           |	"language": "unknown",
-                           |	"results": [
-                           |		{
-                           |			"id": 5,
-                           |			"title": {
-                           |				"title": "Min bit av himmelen",
-                           |				"language": "unknown"
-                           |			},
-                           |			"url": "http://0.0.0.0/audio-api/v1/audio/5",
-                           |			"license": "by-sa",
-                           |			"supportedLanguages": [
-                           |				"unknown"
-                           |			]
-                           |		}
-                           |  ]
-                           |}""".stripMargin
-  val audioResultBody = """{
-                          |	"id": 5,
-                          |	"revision": 2,
-                          |	"title": {
-                          |		"title": "Min bit av himmelen",
-                          |		"language": "unknown"
-                          |	},
-                          |	"audioFile": {
-                          |		"url": "http://0.0.0.0/audio/files/min_bit_av_himmelen_klipp.mp3",
-                          |		"mimeType": "audio/mpeg",
-                          |		"fileSize": 7426944,
-                          |		"language": "unknown"
-                          |	},
-                          |	"copyright": {
-                          |		"license": {
-                          |			"license": "by-sa",
-                          |			"description": "Creative Commons Attribution-ShareAlike 2.0 Generic",
-                          |			"url": "https://creativecommons.org/licenses/by-sa/2.0/"
-                          |		},
-                          |		"creators": [
-                          |			{
-                          |				"type": "Writer",
-                          |				"name": "Sheida Jahanbin"
-                          |			}
-                          |		],
-                          |		"processors": [],
-                          |		"rightsholders": [
-                          |			{
-                          |				"type": "Supplier",
-                          |				"name": "NRK"
-                          |			}
-                          |		]
-                          |	},
-                          |	"tags": {
-                          |		"tags": [
-                          |			"radio documentary",
-                          |			"radio journalist",
-                          |			"sheida jahanbin"
-                          |		],
-                          |		"language": "unknown"
-                          |	},
-                          |	"supportedLanguages": [
-                          |		"unknown"
-                          |	]
-                          |}""".stripMargin
+  val audioResultBody = AudioMetaInformation(
+    id = 5,
+    revision = 2,
+    title = Title("Min bit av himmelen", "unknown"),
+    audioFile = Audio(
+      url = "http://0.0.0.0/audio/files/min_bit_av_himmelen_klipp.mp3",
+      mimeType = "audio/mpeg",
+      fileSize = 7426944,
+      language = "unknown"
+    ),
+    copyright = Copyright(
+      license = License("by-sa", Some("Creative Commons Attribution-ShareAlike 2.0 Generic"), Some("https://creativecommons.org/licenses/by-sa/2.0/")),
+      origin = None,
+      creators = List(Author("Writer", "Sheida Jahanbin")),
+      processors = List(),
+      rightsholders = List(Author("Supplier", "NRK")),
+      agreementId = None,
+      validFrom = None,
+      validTo = None
+    ),
+    tags = Tag(List(
+      "radio documentary",
+      "radio journalist",
+      "sheida jahanbin"
+    ), "unknown"),
+    supportedLanguages = List("unknown")
+  )
+
+  val audioSearchBody = SearchResult(
+    totalCount = 1,
+    page = 1,
+    pageSize = 10,
+    language = "unknown",
+    results = List(
+      AudioSummary(
+        id = 5,
+        title = Title("Min bit av himmelen", "unknown"),
+        url = "http://0.0.0.0/audio-api/v1/audio/5",
+        license = "by-sa",
+        supportedLanguages = List(
+          "unknown"
+        )
+      )
+
+    )
+  )
+
   val httpResponseMock: HttpResponse[String] = mock[HttpResponse[String]]
 
   lazy val controller = new HealthController {
@@ -89,7 +75,7 @@ class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraF
 
   test("that url is fetched properly") {
     val expectedUrl = "http://0.0.0.0/audio-api/v1/audio/5"
-    val (url, totalCount) = controller.getAudioUrl(audioSearchBody)
+    val (url, totalCount) = controller.getAudioUrl(write(audioSearchBody))
 
     url should equal(Some(expectedUrl))
     totalCount should equal(1)
@@ -97,7 +83,7 @@ class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraF
 
   test("that /health returns 200 on success") {
     when(httpResponseMock.code).thenReturn(200)
-    when(httpResponseMock.body).thenReturn(audioSearchBody).thenReturn(audioResultBody)
+    when(httpResponseMock.body).thenReturn(write(audioSearchBody)).thenReturn(write(audioResultBody))
 
     get("/") {
       status should equal (200)
@@ -106,7 +92,7 @@ class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraF
 
   test("that /health returns 500 on failure") {
     when(httpResponseMock.code).thenReturn(500)
-    when(httpResponseMock.body).thenReturn(audioSearchBody).thenReturn(audioResultBody)
+    when(httpResponseMock.body).thenReturn(write(audioSearchBody)).thenReturn(write(audioResultBody))
 
     get("/") {
       status should equal(500)
@@ -114,20 +100,20 @@ class HealthControllerTest extends UnitSuite with TestEnvironment with ScalatraF
   }
 
   test("that /health returns 200 on no images") {
-    val noImageBody = s"""{
-                         |	"totalCount": 0,
-                         |	"page": 1,
-                         |	"pageSize": 10,
-                         |  "language": "unknown",
-                         |  "results": []
-                         |}""".stripMargin
+    val noImageBody = SearchResult(
+      totalCount = 0,
+      page = 1,
+      pageSize = 10,
+      language = "unknown",
+      results = List()
+    )
     val notFoundBody = s"""{
                           |	"code": "NOT_FOUND",
                           |	"description": "Audio with id 51231 not found",
                           |	"occuredAt": "2017-11-24T14:00:22Z"
                           |}""".stripMargin
     when(httpResponseMock.code).thenReturn(200).thenReturn(404)
-    when(httpResponseMock.body).thenReturn(noImageBody).thenReturn(notFoundBody)
+    when(httpResponseMock.body).thenReturn(write(noImageBody)).thenReturn(notFoundBody)
 
     get("/") {
       status should equal(200)
