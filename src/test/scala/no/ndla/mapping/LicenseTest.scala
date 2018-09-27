05abe75b0acc9bc0b629839da3c4f139c0bfd144
Henrik Nårstad <henrik.narstad@knowit.no>
2018-09-27 14:02:48 +0200

    introduce constants for license codes #1100

--- src/test/scala/no/ndla/mapping/LicenseTest.scala
+++ src/test/scala/no/ndla/mapping/LicenseTest.scala
@@ -10,7 +10,7 @@ package no.ndla.mapping
 
 class LicenseTest extends UnitSuite {
   test("getLicense returns a LicenseDefinition if input code is vaid") {
-    val expectedResult = Some(LicenseDefinition("CC-BY-4.0", "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/")))
+    val expectedResult = Some(LicenseDefinition(License.CC_BY, "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/")))
     License.getLicense("CC-BY-4.0") should equal(expectedResult)
   }
 
@@ -19,9 +19,9 @@ class LicenseTest extends UnitSuite {
   }
 
   test("getLicenses returns a list of all licenses") {
-    val byLicense = LicenseDefinition("CC-BY-4.0", "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/"))
+    val byLicense = LicenseDefinition(License.CC_BY, "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/"))
 
     License.getLicenses.size should equal(9)
-    License.getLicenses.find(_.license == "CC-BY-4.0").get should equal (byLicense)
+    License.getLicenses.find(_.license.toString == "CC-BY-4.0").get should equal (byLicense)
   }
 }
