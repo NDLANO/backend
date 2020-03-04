113f785a1926ebf9c5d6e7afce52ebc7155bc17a
Jonas Natten <jonas.natten@knowit.no>
2020-03-04 12:46:35 +0100

    Enables cross compiling and releasing for scala 2.12 and 2.13

--- src/test/scala/no/ndla/mapping/LicenseTest.scala
+++ src/test/scala/no/ndla/mapping/LicenseTest.scala
@@ -10,7 +10,10 @@ package no.ndla.mapping
 
 class LicenseTest extends UnitSuite {
   test("getLicense returns a LicenseDefinition if input code is vaid") {
-    val expectedResult = Some(LicenseDefinition(License.CC_BY, "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/")))
+    val expectedResult = Some(
+      LicenseDefinition(License.CC_BY,
+                        "Creative Commons Attribution 4.0 International",
+                        Some("https://creativecommons.org/licenses/by/4.0/")))
     License.getLicense("CC-BY-4.0") should equal(expectedResult)
   }
 
@@ -19,9 +22,14 @@ class LicenseTest extends UnitSuite {
   }
 
   test("getLicenses returns a list of all licenses") {
-    val byLicense = LicenseDefinition(License.CC_BY, "Creative Commons Attribution 4.0 International", Some("https://creativecommons.org/licenses/by/4.0/"))
+    val byLicense =
+      LicenseDefinition(License.CC_BY,
+                        "Creative Commons Attribution 4.0 International",
+                        Some("https://creativecommons.org/licenses/by/4.0/"))
 
     License.getLicenses.size should equal(9)
-    License.getLicenses.find(_.license.toString == "CC-BY-4.0").get should equal (byLicense)
+    License.getLicenses
+      .find(_.license.toString == "CC-BY-4.0")
+      .get should equal(byLicense)
   }
 }
