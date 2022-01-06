4b82ab6d7e39fca3402dfb1a3691d7922a2ed72b
Jonas Natten <jonas.natten@knowit.no>
2022-01-06 13:51:55 +0100

    Bump `scala-uri` for oembed-proxy

--- project/oembedproxy.scala
+++ project/oembedproxy.scala
@@ -9,12 +9,12 @@ object oembedproxy extends Module {
   override val MainClass: Option[String] = Some("no.ndla.oembedproxy.JettyLauncher")
   lazy val dependencies: Seq[ModuleID] = withLogging(
     Seq(
+      scalaUri,
       "org.eclipse.jetty" % "jetty-webapp" % JettyV % "container;compile",
       "org.eclipse.jetty" % "jetty-plus" % JettyV % "container",
       "javax.servlet" % "javax.servlet-api" % "3.1.0" % "container;provided;test",
       "org.json4s" %% "json4s-native" % Json4SV,
       "org.scalaj" %% "scalaj-http" % "2.4.2",
-      "io.lemonlabs" %% "scala-uri" % "1.5.1",
       "org.jsoup" % "jsoup" % "1.11.3",
       "org.scalatest" %% "scalatest" % ScalaTestV % "test",
       "org.mockito" %% "mockito-scala" % MockitoV % "test",
