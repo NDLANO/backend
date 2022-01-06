8c8bb599c2d0cf7f651848c86980cd4e341ade7e
Jonas Natten <jonas.natten@knowit.no>
2022-01-06 18:22:03 +0100

    Add `search` library to share search related code
    This library will be useful to share logic relating to elasticsearch and
    searching in general.

---
+++ project/searchlib.scala
@@ -0,0 +1,23 @@
+import Dependencies.versions.{ElasticsearchV, elastic4sAWS, elastic4sCore, elastic4sEmbedded, elastic4sHttp, scalaUri}
+import sbt.Keys._
+import sbt._
+import com.scalatsi.plugin.ScalaTsiPlugin
+
+object searchlib extends Module {
+  lazy val dependencies: Seq[ModuleID] = Seq(
+    "org.elasticsearch" % "elasticsearch" % ElasticsearchV,
+    elastic4sCore,
+    elastic4sHttp,
+    elastic4sAWS,
+    elastic4sEmbedded,
+    scalaUri
+  )
+  override lazy val settings: Seq[Def.Setting[_]] = Seq(
+    name := "search",
+    libraryDependencies ++= dependencies
+  ) ++
+    commonSettings ++
+    fmtSettings
+
+  override lazy val disablePlugins = Seq(ScalaTsiPlugin)
+}
---
+++ search/src/main/scala/no/ndla/search/NdlaSearchException.scala
@@ -0,0 +1,14 @@
+package no.ndla.search
+
+import com.sksamuel.elastic4s.http.RequestFailure
+
+case class NdlaSearchException(rf: RequestFailure)
+    extends RuntimeException(
+      s"""
+       |index: ${rf.error.index.getOrElse("Error did not contain index")}
+       |reason: ${rf.error.reason}
+       |body: ${rf.body}
+       |shard: ${rf.error.shard.getOrElse("Error did not contain shard")}
+       |type: ${rf.error.`type`}
+   """.stripMargin
+    )
