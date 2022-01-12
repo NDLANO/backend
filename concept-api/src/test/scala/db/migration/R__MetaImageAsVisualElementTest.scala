d6fe822c1984bffae4401417e4824c792fdb3559
Jonas Natten <jonas.natten@knowit.no>
2022-01-12 08:48:49 +0100

    concept-api: Adds test for not overriding existing visual elements in repeatable migration

--- concept-api/src/test/scala/db/migration/R__MetaImageAsVisualElementTest.scala
+++ concept-api/src/test/scala/db/migration/R__MetaImageAsVisualElementTest.scala
@@ -40,4 +40,26 @@ class R__MetaImageAsVisualElementTest extends UnitSuite with TestEnvironment {
     migration.convertToNewConcept(old) should be(expected)
   }
 
+  test("Meta images should not override existing visual elements") {
+    val oldMetaImageNb = """{"imageId":"1","altText":"alt","language":"nb"}"""
+    val oldMetaImageNn = """{"imageId":"1","altText":"alt2","language":"nn"}"""
+    val oldVisualElementString =
+      """<embed data-resource_id=\"123\" data-resource=\"image\" data-alt=\"somealt\" data-size=\"full\" data-align=\"\" />"""
+
+    val oldVisualElement = s"""{"visualElement":"$oldVisualElementString","language":"nb"}"""
+
+    val old =
+      s"""{"metaImage":[$oldMetaImageNb,$oldMetaImageNn],"visualElement":[$oldVisualElement]}"""
+
+    val newVisualElementString =
+      """<embed data-resource=\"image\" data-resource_id=\"1\" data-alt=\"alt2\" data-size=\"full\" data-align=\"\" />"""
+
+    val expectedNewVisualElement = s"""{"visualElement":"$newVisualElementString","language":"nn"}"""
+
+    val expected =
+      s"""{"metaImage":[$oldMetaImageNb,$oldMetaImageNn],"visualElement":[$oldVisualElement,$expectedNewVisualElement]}"""
+    migration.convertToNewConcept(old) should be(expected)
+
+  }
+
 }
