c1a52d48ebf6d38ff191b634b14d265da151ffaa
Gunnar Velle <gunnar.velle@gmail.com>
2022-02-04 08:25:56 +0100

    Return 404 when remote returns 404

--- oembed-proxy/src/main/scala/no/ndla/oembedproxy/controller/OEmbedProxyController.scala
+++ oembed-proxy/src/main/scala/no/ndla/oembedproxy/controller/OEmbedProxyController.scala
@@ -15,7 +15,7 @@ import no.ndla.oembedproxy.model._
 import no.ndla.oembedproxy.service.OEmbedServiceComponent
 import org.json4s.ext.EnumNameSerializer
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.{BadGateway, BadRequest, InternalServerError, NotImplemented, ScalatraServlet}
+import org.scalatra.{BadGateway, BadRequest, InternalServerError, NotFound, NotImplemented, ScalatraServlet}
 import org.scalatra.json.NativeJsonSupport
 import org.scalatra.swagger.{ResponseMessage, Swagger, SwaggerSupport, SwaggerSupportSyntax}
 import org.scalatra.util.NotNothing
@@ -35,12 +35,13 @@ trait OEmbedProxyController {
     protected implicit override val jsonFormats: Formats = DefaultFormats
 
     protected val applicationDescription =
-      "API for accessing Learningpaths from ndla.no."
+      "API wrapper for oembed.com adding support for ndla.no."
 
     registerModel[Error]()
 
     val response400: ResponseMessage = ResponseMessage(400, "Validation error", Some("Error"))
     val response401: ResponseMessage = ResponseMessage(401, "Unauthorized")
+    val response404: ResponseMessage = ResponseMessage(404, "Url not found")
     val response500: ResponseMessage = ResponseMessage(500, "Unknown error", Some("Error"))
 
     val response501: ResponseMessage =
@@ -81,6 +82,9 @@ trait OEmbedProxyController {
         BadRequest(Error(Error.PARAMETER_MISSING, pme.getMessage))
       case pnse: ProviderNotSupportedException =>
         NotImplemented(Error(Error.PROVIDER_NOT_SUPPORTED, pnse.getMessage))
+      case hre: HttpRequestException if hre.is404 =>
+        logger.info(s"Could not fetch remote: '${hre.getMessage}'")
+        NotFound(Error(Error.REMOTE_ERROR, hre.getMessage))
       case hre: HttpRequestException =>
         val msg = hre.httpResponse.map(response =>
           s": Received '${response.code}' '${response.statusLine}'. Body was '${response.body}'")
