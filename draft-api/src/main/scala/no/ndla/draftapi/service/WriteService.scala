5bcd05230e56a34c5c394bb51b86cffead3aa54d
Jonas Natten <jonas.natten@knowit.no>
2022-03-02 13:29:08 +0100

    Fix that language affects fallback when updating null-article as well.
    f752cc5d9d7fd32e008f3ce338d8ecd323249316 fixes this for regular
    existing articles. This should not be different.

--- draft-api/src/main/scala/no/ndla/draftapi/service/WriteService.scala
+++ draft-api/src/main/scala/no/ndla/draftapi/service/WriteService.scala
@@ -545,7 +545,7 @@ trait WriteService {
         oldNdlaCreatedDate: Option[Date],
         oldNdlaUpdatedDate: Option[Date],
         importId: Option[String]
-    ): Try[Article] =
+    ): Try[api.Article] =
       for {
         convertedArticle <- converterService.toDomainArticle(
           articleId,
@@ -555,12 +555,12 @@ trait WriteService {
           oldNdlaCreatedDate,
           oldNdlaUpdatedDate
         )
-        articleWithStatusT <- converterService
-          .updateStatus(DRAFT, convertedArticle, user, false)
+        articleWithStatus <- converterService
+          .updateStatus(DRAFT, convertedArticle, user, isImported = false)
           .attempt
           .unsafeRunSync()
           .toTry
-        articleWithStatus <- articleWithStatusT
+          .flatten
         updatedArticle <- updateArticle(
           toUpdate = articleWithStatus,
           importId = importId,
@@ -574,7 +574,8 @@ trait WriteService {
         )
         apiArticle <- converterService.toApiArticle(
           readService.addUrlsOnEmbedResources(updatedArticle),
-          updatedApiArticle.language.getOrElse(UnknownLanguage.toString)
+          updatedApiArticle.language.getOrElse(UnknownLanguage.toString),
+          updatedApiArticle.language.isEmpty
         )
       } yield apiArticle
 
