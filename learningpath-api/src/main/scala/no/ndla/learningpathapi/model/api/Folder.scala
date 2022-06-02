d0c7682b510dcdd6c4f85f92e9292daee93856f1
Bartek <bartosz.tracz@knowit.no>
2022-06-02 10:27:14 +0200

    adding comments for special case in generatingTypescript for Folder type

--- learningpath-api/src/main/scala/no/ndla/learningpathapi/model/api/Folder.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/model/api/Folder.scala
@@ -17,8 +17,6 @@ import com.scalatsi._
 
 import scala.annotation.unused
 
-sealed trait FolderData {}
-
 case class Folder(
     @(ApiModelProperty @field)(description = "Unique ID of the folder") id: Long,
     @(ApiModelProperty @field)(description = "Folder name") name: String,
@@ -27,18 +25,24 @@ case class Folder(
     @(ApiModelProperty @field)(description = "List of other folders and resources") data: List[FolderData]
 ) extends FolderData
 
-object FolderData {
-  implicit val folderDataUnion: TSUnion                 = TSUnion.of(Folder.resource.get, Folder.folderTSI.get)
-  implicit val folderDataAlias: TSNamedType[FolderData] = TSType.alias[FolderData]("IFolderData", folderDataUnion)
-}
-
+// 1: This object is needed for generating recursive Folder typescript type.
 object Folder {
   implicit val resource: TSIType[Resource] = TSType.fromCaseClass[Resource]
   implicit val folderTSI: TSIType[Folder] = {
     @unused
+    // 2: We are saying here that there is no need for scala-tsi to generate IFolderData type automatically.
+    // We assure scala-tsi that we are getting IFolderData from external source. We point that source to FolderData.
     implicit val folderData: TSNamedType[FolderData] = TSType.external[FolderData]("IFolderData")
     TSType.fromCaseClass[Folder]
   }
+}
+
+sealed trait FolderData {}
+object FolderData       {
+  // 3: After being redirected here from TSType.external we are manually making the union of FolderData,
+  // with Resource and Folder. After that we alias it as IFolderData so that scala-tsi can incorporate it.
+  implicit val folderDataUnion: TSUnion                 = TSUnion.of(Folder.resource.get, Folder.folderTSI.get)
+  implicit val folderDataAlias: TSNamedType[FolderData] = TSType.alias[FolderData]("IFolderData", folderDataUnion)
 }
 
 case class NewFolder(
