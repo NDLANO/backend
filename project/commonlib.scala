1fe9a8ad3b0b6d226f8556571e7e2407dd15086e
Bartek <bartosz.tracz@knowit.no>
2022-08-17 10:05:02 +0200

    added enumeratum and enumeratumJson4s as dependency to common package

--- project/commonlib.scala
+++ project/commonlib.scala
@@ -6,6 +6,8 @@ import com.scalatsi.plugin.ScalaTsiPlugin
 object commonlib extends Module {
   lazy val dependencies: Seq[ModuleID] = withLogging(
     Seq(
+      enumeratum,
+      enumeratumJson4s,
       "org.json4s"   %% "json4s-native"     % Json4SV,
       "org.json4s"   %% "json4s-ext"        % Json4SV,
       "javax.servlet" % "javax.servlet-api" % JavaxServletV
