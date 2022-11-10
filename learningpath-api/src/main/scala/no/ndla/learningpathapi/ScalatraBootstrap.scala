042316c581796dd181fb34309cea75727c7d4540
Jonas Natten <jonas.natten@knowit.no>
2022-11-10 10:43:15 +0100

    learningpath-api: Start using `NdlaScalatraServer`

--- learningpath-api/src/main/scala/no/ndla/learningpathapi/ScalatraBootstrap.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/ScalatraBootstrap.scala
@@ -8,13 +8,11 @@
 
 package no.ndla.learningpathapi
 
-import org.scalatra.LifeCycle
+import no.ndla.common.scalatra.NdlaScalatraBootstrapBase
 import javax.servlet.ServletContext
 
-class ScalatraBootstrap extends LifeCycle {
-
-  override def init(context: ServletContext): Unit = {
-    val componentRegistry = context.getAttribute("ComponentRegistry").asInstanceOf[ComponentRegistry]
+class ScalatraBootstrap extends NdlaScalatraBootstrapBase[ComponentRegistry] {
+  override def ndlaInit(context: ServletContext, componentRegistry: ComponentRegistry): Unit = {
     context.mount(componentRegistry.learningpathControllerV2, "/learningpath-api/v2/learningpaths", "learningpaths_v2")
     context.mount(componentRegistry.internController, "/intern")
     context.mount(componentRegistry.resourcesApp, "/learningpath-api/api-docs")
