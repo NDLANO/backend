e4b4b10c1eb1d0e8964e02e7bd883acea0d98669
Jonas Natten <jonas.natten@knowit.no>
2023-04-19 08:46:14 +0200

    oembed-proxy: Add test for 500 controller case as well :^)

--- oembed-proxy/src/test/scala/no/ndla/oembedproxy/controller/OEmbedProxyControllerTest.scala
+++ oembed-proxy/src/test/scala/no/ndla/oembedproxy/controller/OEmbedProxyControllerTest.scala
@@ -43,7 +43,7 @@ class OEmbedProxyControllerTest extends UnitSuite with TestEnvironment {
     blockUntil(() => serverReady)
   }
 
-  val oembed = OEmbed(
+  val oembed: OEmbed = OEmbed(
     `type` = "rich",
     version = "1.0",
     title = Some("Title"),
@@ -82,5 +82,17 @@ class OEmbedProxyControllerTest extends UnitSuite with TestEnvironment {
     val url           = uri"http://localhost:$serverPort/oembed-proxy/v1/oembed?$requestParams"
     val response      = simpleHttpClient.send(quickRequest.get(url))
     response.code.code should be(404)
+  }
+
+  test("h5p url should return 500 if something bad happens") {
+    val exception = new HttpRequestException("bad", None) {
+      override def is404: Boolean = false
+    }
+    when(oEmbedService.get(anyString, any[Option[String]], any[Option[String]])).thenReturn(Failure(exception))
+    when(clock.now()).thenCallRealMethod()
+    val requestParams = Map("url" -> "https://h5p-test.ndla.no/resource/bae851c6-0e98-411d-bd92-ec2ab8fce730")
+    val url           = uri"http://localhost:$serverPort/oembed-proxy/v1/oembed?$requestParams"
+    val response      = simpleHttpClient.send(quickRequest.get(url))
+    response.code.code should be(500)
   }
 }
