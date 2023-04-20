6a50b348c2158ad3587f55fff6713ba9d188fcfb
Jonas Natten <jonas.natten@knowit.no>
2023-04-20 14:40:08 +0200

    Improve logging in error handling

--- network/src/main/scala/no/ndla/network/tapir/Routes.scala
+++ network/src/main/scala/no/ndla/network/tapir/Routes.scala
@@ -9,22 +9,27 @@ package no.ndla.network.tapir
 
 import cats.data.Kleisli
 import cats.effect.IO
-import com.typesafe.scalalogging.StrictLogging
 import io.circe.generic.auto._
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.headers.`Content-Type`
 import org.http4s.server.Router
 import org.http4s.{Headers, HttpRoutes, MediaType, Request, Response}
+import org.log4s.getLogger
+import sttp.model.StatusCode
+import sttp.monad.MonadError
 import sttp.tapir.generic.auto.schemaForCaseClass
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.http4s.{Http4sServerInterpreter, Http4sServerOptions}
 import sttp.tapir.server.interceptor.decodefailure.DefaultDecodeFailureHandler
+import sttp.tapir.server.interceptor.exception.{ExceptionContext, ExceptionHandler}
 import sttp.tapir.server.model.ValuedEndpointOutput
+import sttp.tapir.statusCode
 
 trait Routes {
   this: Service with NdlaMiddleware with TapirErrorHelpers =>
 
-  object Routes extends StrictLogging {
+  object Routes {
+    val logger = getLogger
     private def buildBindings(routes: List[Service]): List[(String, HttpRoutes[IO])] = {
       val (docServices, noDocServices) = routes.partitionMap {
         case swaggerService: SwaggerService  => Left(swaggerService)
@@ -36,8 +41,13 @@ trait Routes {
       noDocServices.map(_.getBinding) :+ swaggerBinding
     }
 
-    private def failureResponse(error: String): ValuedEndpointOutput[_] = {
-      logger.error(s"Failure handler got: $error")
+    private def failureResponse(error: String, exception: Option[Throwable]): ValuedEndpointOutput[_] = {
+      val logMsg = s"Failure handler got: $error"
+      exception match {
+        case Some(ex) => logger.error(ex)(logMsg)
+        case None     => logger.error(logMsg)
+      }
+
       ValuedEndpointOutput(jsonBody[ErrorBody], ErrorHelpers.generic)
     }
 
@@ -45,11 +55,23 @@ trait Routes {
       ValuedEndpointOutput(jsonBody[ErrorBody], ErrorHelpers.badRequest(failureMsg))
     })
 
+    private case class NdlaExceptionHandler() extends ExceptionHandler[IO] {
+      override def apply(ctx: ExceptionContext)(implicit monad: MonadError[IO]): IO[Option[ValuedEndpointOutput[_]]] = {
+        monad.unit(
+          Some(
+            failureResponse("Internal server error", Some(ctx.e))
+              .prepend(statusCode, StatusCode.InternalServerError)
+          )
+        )
+      }
+    }
+
     private def swaggerServicesToRoutes(services: List[SwaggerService]): HttpRoutes[IO] = {
       val swaggerEndpoints = services.flatMap(_.builtEndpoints)
       val options = Http4sServerOptions
         .customiseInterceptors[IO]
-        .defaultHandlers(failureResponse)
+        .defaultHandlers(err => failureResponse(err, None))
+        .exceptionHandler(NdlaExceptionHandler())
         .decodeFailureHandler(decodeFailureHandler)
         .options
       Http4sServerInterpreter[IO](options).toRoutes(swaggerEndpoints)
