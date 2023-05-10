c08880c42e07668a25c3a68777144d9f93d861aa
Bartek <bartosz.tracz@knowit.no>
2023-05-10 12:18:01 +0200

    concept-api: add conceptType and wordList fields to concept

---
+++ concept-api/src/main/scala/no/ndla/conceptapi/model/domain/ConceptType.scala
@@ -0,0 +1,28 @@
+/*
+ * Part of NDLA concept-api.
+ * Copyright (C) 2023 NDLA
+ *
+ * See LICENSE
+ */
+
+package no.ndla.conceptapi.model.domain
+
+import no.ndla.conceptapi.model.api.InvalidStatusException
+import scala.util.{Failure, Success, Try}
+
+object ConceptType extends Enumeration {
+  val CONCEPT: ConceptType.Value   = Value("concept")
+  val WORDCLASS: ConceptType.Value = Value("wordclass")
+
+  def all: Seq[String]                                      = ConceptType.values.map(_.toString).toSeq
+  def valueOf(s: String): Option[ConceptType.Value]         = ConceptType.values.find(_.toString == s)
+  def valueOf(s: Option[String]): Option[ConceptType.Value] = s.flatMap(valueOf)
+
+  def valueOfOrError(s: String): Try[ConceptType.Value] = {
+    valueOf(s) match {
+      case None =>
+        Failure(InvalidStatusException(s"'$s' is not a valid concept type. Valid options are ${all.mkString(", ")}."))
+      case Some(conceptType) => Success(conceptType)
+    }
+  }
+}
