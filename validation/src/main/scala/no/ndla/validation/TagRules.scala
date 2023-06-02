b870a194c8578878b2da5caeccc9cbda8eb7356e
MaPoKen <larssoerlie96@gmail.com>
2023-06-02 14:36:46 +0200

    Validation: Added background to grid color

--- validation/src/main/scala/no/ndla/validation/TagRules.scala
+++ validation/src/main/scala/no/ndla/validation/TagRules.scala
@@ -108,6 +108,7 @@ object TagAttributes extends Enumeration {
   val DataSubtitle: TagAttributes.Value    = Value("data-subtitle")
   val DataColumns: TagAttributes.Value     = Value("data-columns")
   val DataBorder: TagAttributes.Value      = Value("data-border")
+  val DataBackground: TagAttributes.Value  = Value("data-background")
   val XMLNsAttribute: TagAttributes.Value  = Value("xmlns")
 
   val Href: TagAttributes.Value    = Value("href")
