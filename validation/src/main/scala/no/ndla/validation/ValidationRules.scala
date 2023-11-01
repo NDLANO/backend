259eee137a4613a0a4ee57fefc6e4514a5525f63
Jonas Natten <jonas.natten@knowit.no>
2023-11-01 13:17:32 +0100

    validation: Slight refactor of the deserialization to avoid mistakes
    We experienced a small bug when deserializing `mathml-rules.json`
    because we missed a few attributes. This refactor avoids that by using
    types rather than `Any` and casting.
    
    Also adds tests that will load the json into memory to check that
    deserializing doesn't fail.

--- validation/src/main/scala/no/ndla/validation/ValidationRules.scala
+++ validation/src/main/scala/no/ndla/validation/ValidationRules.scala
@@ -1,14 +1,22 @@
 package no.ndla.validation
-import org.json4s.DefaultFormats
+import enumeratum.Json4s
+import no.ndla.validation.model.{HtmlRulesFile, MathMLRulesFile}
+import org.json4s.{DefaultFormats, Formats}
 import org.json4s.native.JsonMethods.parse
 
 import scala.io.Source
 
 object ValidationRules {
 
-  def htmlRulesJson: Map[String, Any]     = convertJsonStr(Source.fromResource("html-rules.json").mkString)
   def embedTagRulesJson: Map[String, Any] = convertJsonStr(Source.fromResource("embed-tag-rules.json").mkString)
-  def mathMLRulesJson: Map[String, Any]   = convertJsonStr(Source.fromResource("mathml-rules.json").mkString)
+  def htmlRulesJson: HtmlRulesFile = {
+    implicit val formats: Formats = org.json4s.DefaultFormats + Json4s.serializer(TagAttribute)
+    parse(Source.fromResource("html-rules.json").mkString).extract[HtmlRulesFile]
+  }
+  def mathMLRulesJson: MathMLRulesFile = {
+    implicit val formats: Formats = org.json4s.DefaultFormats + Json4s.serializer(TagAttribute)
+    parse(Source.fromResource("mathml-rules.json").mkString).extract[MathMLRulesFile]
+  }
 
   private def convertJsonStr(jsonStr: String): Map[String, Any] = {
     implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
