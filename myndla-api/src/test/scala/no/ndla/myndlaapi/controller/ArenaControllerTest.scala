2a14c8b33ee8aed8b364515bba0ca02da9393889
Jonas Natten <jonas.natten@knowit.no>
2023-12-13 12:53:51 +0100

    myndla-api: Add token parsing tests for arenacontroller

--- myndla-api/src/test/scala/no/ndla/myndlaapi/controller/ArenaControllerTest.scala
+++ myndla-api/src/test/scala/no/ndla/myndlaapi/controller/ArenaControllerTest.scala
@@ -9,17 +9,15 @@ package no.ndla.myndlaapi.controller
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import no.ndla.common.model.NDLADate
-import no.ndla.myndla.model.api.config.{ConfigMeta, ConfigMetaValue}
-import no.ndla.myndla.model.domain.config.ConfigKey
-import no.ndla.myndlaapi.TestData.{adminAndWriteScopeClientToken, adminScopeClientToken}
-import no.ndla.myndlaapi.{Eff, TestEnvironment}
+import no.ndla.common.errors.AccessDeniedException
+import no.ndla.myndla.model.domain.{MyNDLAUser, UserRole}
+import no.ndla.myndlaapi.model.arena.api.{Paginated, Topic}
+import no.ndla.myndlaapi.{Eff, TestData, TestEnvironment}
 import no.ndla.network.tapir.Service
-import no.ndla.network.tapir.auth.TokenUser
 import no.ndla.scalatestsuite.UnitTestSuite
 import sttp.client3.quick._
 
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 class ArenaControllerTest extends UnitTestSuite with TestEnvironment {
   val serverPort: Int = findFreePort
@@ -32,7 +30,120 @@ class ArenaControllerTest extends UnitTestSuite with TestEnvironment {
     Thread.sleep(1000)
   }
 
-  test("That token parsing works") { ??? }
-  test("That admin token parsing works") { ??? }
-  test("That recent endpoint works along with id endpoint for topics") { ??? }
+  override def beforeEach(): Unit = {
+    resetMocks()
+    when(clock.now()).thenReturn(TestData.today)
+  }
+
+  val feideToken = "aec48787-36b7-4d04-8c11-40e374256f1e"
+  val feideId    = "someid"
+
+  val testUser = MyNDLAUser(
+    id = 1,
+    feideId = feideId,
+    favoriteSubjects = Seq.empty,
+    userRole = UserRole.EMPLOYEE,
+    lastUpdated = TestData.today,
+    organization = "yap",
+    groups = Seq.empty,
+    username = "username",
+    displayName = "displayName",
+    email = "some@example.com",
+    arenaEnabled = true,
+    arenaAdmin = Some(false),
+    shareName = false
+  )
+
+  test("That feide token parsing works if token present") {
+    when(userService.getArenaEnabledUser(eqTo(Some(feideToken)))).thenReturn(Success(testUser))
+    when(arenaReadService.getRecentTopics(any, any, any)(any)).thenReturn(
+      Success(
+        Paginated[Topic](
+          items = List.empty,
+          page = 1,
+          pageSize = 10,
+          totalCount = 0
+        )
+      )
+    )
+    val request = quickRequest
+      .get(uri"http://localhost:$serverPort/myndla-api/v1/arena/topics/recent")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+    val response = simpleHttpClient.send(request)
+
+    verify(arenaReadService, times(1)).getRecentTopics(any, any, eqTo(testUser))(any)
+    response.code.code should be(200)
+  }
+
+  test("That feide token parsing returns forbidden if token present, but no user") {
+    when(userService.getArenaEnabledUser(eqTo(Some(feideToken)))).thenReturn(Failure(AccessDeniedException.forbidden))
+    when(arenaReadService.getRecentTopics(any, any, any)(any)).thenReturn(
+      Success(
+        Paginated[Topic](
+          items = List.empty,
+          page = 1,
+          pageSize = 10,
+          totalCount = 0
+        )
+      )
+    )
+    val request = quickRequest
+      .get(uri"http://localhost:$serverPort/myndla-api/v1/arena/topics/recent")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+    val response = simpleHttpClient.send(request)
+
+    verify(arenaReadService, times(0)).getRecentTopics(any, any, eqTo(testUser))(any)
+    response.code.code should be(403)
+  }
+
+  test("That feide token parsing returns unauthorized if no token present") {
+    when(userService.getArenaEnabledUser(eqTo(None))).thenReturn(Failure(AccessDeniedException.unauthorized))
+    when(arenaReadService.getRecentTopics(any, any, any)(any)).thenReturn(
+      Success(
+        Paginated[Topic](
+          items = List.empty,
+          page = 1,
+          pageSize = 10,
+          totalCount = 0
+        )
+      )
+    )
+    val request  = quickRequest.get(uri"http://localhost:$serverPort/myndla-api/v1/arena/topics/recent")
+    val response = simpleHttpClient.send(request)
+
+    verify(arenaReadService, times(0)).getRecentTopics(any, any, eqTo(testUser))(any)
+    response.code.code should be(401)
+  }
+
+  test("That admin feide token parsing returns 403 if no admin") {
+    when(userService.getArenaEnabledUser(eqTo(Some(feideToken))))
+      .thenReturn(Success(testUser.copy(arenaAdmin = Some(false))))
+    when(arenaReadService.deleteCategory(eqTo(1L), eqTo(testUser))(any)).thenReturn(Success(()))
+
+    val request = quickRequest
+      .delete(uri"http://localhost:$serverPort/myndla-api/v1/arena/categories/1")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+
+    val response = simpleHttpClient.send(request)
+
+    verify(arenaReadService, times(0)).deleteCategory(any, eqTo(testUser))(any)
+    response.code.code should be(403)
+  }
+
+  test("That admin feide token parsing returns 200 if admin") {
+    val adminUser = testUser.copy(arenaAdmin = Some(true))
+    when(userService.getArenaEnabledUser(eqTo(Some(feideToken))))
+      .thenReturn(Success(adminUser))
+
+    when(arenaReadService.deleteCategory(eqTo(1L), eqTo(adminUser))(any)).thenReturn(Success(()))
+
+    val request = quickRequest
+      .delete(uri"http://localhost:$serverPort/myndla-api/v1/arena/categories/1")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+
+    val response = simpleHttpClient.send(request)
+
+    verify(arenaReadService, times(1)).deleteCategory(any, eqTo(adminUser))(any)
+    response.code.code should be(200)
+  }
 }
