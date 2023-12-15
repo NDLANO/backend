8c3afdf826c99db54b1569118fd488d317a6b30a
Jonas Natten <jonas.natten@knowit.no>
2023-12-15 15:44:13 +0100

    myndla-api: Refactor most topic endpoints to not include posts
    Only include posts when asking for a single topic to simplify pagination
    reasoning for the clients :^)

--- myndla-api/src/main/scala/no/ndla/myndlaapi/model/arena/api/TopicWithPosts.scala
+++ myndla-api/src/main/scala/no/ndla/myndlaapi/model/arena/api/TopicWithPosts.scala
@@ -11,10 +11,11 @@ import no.ndla.common.model.NDLADate
 import sttp.tapir.Schema.annotations.description
 
 @description("Arena topic data")
-case class Topic(
+case class TopicWithPosts(
     @description("The topics id") id: Long,
     @description("The topics title") title: String,
     @description("Count of posts in the topic") postCount: Long,
+    @description("The posts in the topic") posts: Paginated[Post],
     @description("The post creation date") created: NDLADate,
     @description("The post edit date") updated: NDLADate
 )
