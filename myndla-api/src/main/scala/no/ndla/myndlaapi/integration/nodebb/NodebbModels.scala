f14d3873604efb7bc3faf9337222b3cc64cd3d6f
Jonas Natten <jonas.natten@knowit.no>
2023-12-19 15:53:26 +0100

    myndla-api: Add `/intern/arena/import` to import from nodebb
    Adds a prop `NODEBB_URL` which will be used to import data from nodebb.

---
+++ myndla-api/src/main/scala/no/ndla/myndlaapi/integration/nodebb/NodebbModels.scala
@@ -0,0 +1,78 @@
+/*
+ * Part of NDLA myndla-api
+ * Copyright (C) 2023 NDLA
+ *
+ * See LICENSE
+ */
+
+package no.ndla.myndlaapi.integration.nodebb
+
+case class CategoryInList(
+    cid: Long,
+    name: String,
+    description: String
+)
+
+case class Categories(
+    categories: List[CategoryInList]
+)
+
+case class Owner(
+    uid: Long,
+    username: String,
+    userslug: String,
+    displayname: String
+)
+
+case class TopicInList(
+    cid: Long,
+    tid: Long,
+    uid: Long,
+    title: String,
+    user: Owner,
+    deleted: Long
+)
+
+case class Pagination(
+    currentPage: Long,
+    pageCount: Long
+)
+
+case class Post(
+    pid: Long,
+    tid: Long,
+    uid: Long,
+    content: String,
+    edited: Long,
+    timestamp: Long,
+    user: Owner,
+    deleted: Long
+)
+
+case class SinglePostResponse(response: SinglePost)
+case class SinglePost(
+    pid: Long,
+    tid: Long,
+    uid: Long,
+    content: String
+)
+
+case class SingleTopic(
+    cid: Long,
+    tid: Long,
+    uid: Long,
+    title: String,
+    timestamp: Long,
+    pagination: Pagination,
+    posts: List[Post]
+)
+
+case class SingleCategory(
+    cid: Long,
+    name: String,
+    description: String,
+    pagination: Pagination,
+    topics: List[TopicInList]
+)
+
+case class ImportException(message: String) extends RuntimeException(message)
