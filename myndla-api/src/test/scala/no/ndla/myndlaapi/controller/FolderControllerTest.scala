eabd601686d621bbf2d8c6770a02c308430eacf4
Jonas Natten <jonas.natten@knowit.no>
2024-01-03 17:59:00 +0100

    myndla-api: Fix bug where resources endpoint didn't work
    There was a regression where the "resources" part of the fetch all
    resources endpoint was picked up as a folder-id. Fixed it by reordering
    th endpoints and added two tests to prevent further regressions :^)

---
+++ myndla-api/src/test/scala/no/ndla/myndlaapi/controller/FolderControllerTest.scala
@@ -0,0 +1,102 @@
+/*
+ * Part of NDLA myndla-api
+ * Copyright (C) 2023 NDLA
+ *
+ * See LICENSE
+ */
+
+package no.ndla.myndlaapi.controller
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import no.ndla.common.errors.AccessDeniedException
+import no.ndla.myndla.model.api.Folder
+import no.ndla.myndla.model.domain.{ArenaGroup, MyNDLAUser, UserRole}
+import no.ndla.myndlaapi.model.arena.api.PaginatedTopics
+import no.ndla.myndlaapi.{Eff, TestData, TestEnvironment}
+import no.ndla.network.tapir.Service
+import no.ndla.scalatestsuite.UnitTestSuite
+import sttp.client3.quick._
+
+import java.util.UUID
+import scala.util.{Failure, Success}
+
+class FolderControllerTest extends UnitTestSuite with TestEnvironment {
+  val serverPort: Int = findFreePort
+
+  val controller                            = new FolderController()
+  override val services: List[Service[Eff]] = List(controller)
+
+  override def beforeAll(): Unit = {
+    IO { Routes.startJdkServer(this.getClass.getName, serverPort) {} }.unsafeRunAndForget()
+    Thread.sleep(1000)
+  }
+
+  override def beforeEach(): Unit = {
+    resetMocks()
+    when(clock.now()).thenReturn(TestData.today)
+  }
+
+  val feideToken = "aec48787-36b7-4d04-8c11-40e374256f1e"
+  val feideId    = "someid"
+
+  val testUser = MyNDLAUser(
+    id = 1,
+    feideId = feideId,
+    favoriteSubjects = Seq.empty,
+    userRole = UserRole.EMPLOYEE,
+    lastUpdated = TestData.today,
+    organization = "yap",
+    groups = Seq.empty,
+    username = "username",
+    displayName = "displayName",
+    email = "some@example.com",
+    arenaEnabled = true,
+    arenaGroups = List.empty,
+    shareName = false
+  )
+
+  test("That resources fetching works and doesnt interfere with folder/:id") {
+    when(folderReadService.getAllResources(any, any)).thenReturn(Success(List.empty))
+    val request = quickRequest
+      .get(uri"http://localhost:$serverPort/myndla-api/v1/folders/resources")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+    val response = simpleHttpClient.send(request)
+
+    verify(folderReadService, times(1)).getAllResources(any, any)
+    verify(folderReadService, times(0)).getSingleFolder(any, any, any, any)
+    response.code.code should be(200)
+  }
+
+  test("That fetching single folder works with id") {
+    val someId = UUID.randomUUID()
+    when(folderReadService.getSingleFolder(eqTo(someId), any, any, any)).thenReturn(
+      Success(
+        Folder(
+          id = someId.toString,
+          name = "folderName",
+          status = "private",
+          parentId = None,
+          breadcrumbs = List.empty,
+          subfolders = List.empty,
+          resources = List.empty,
+          rank = Some(1),
+          created = TestData.today,
+          updated = TestData.today,
+          shared = None,
+          description = None,
+          owner = None
+        )
+      )
+    )
+    val request = quickRequest
+      .get(uri"http://localhost:$serverPort/myndla-api/v1/folders/${someId.toString}")
+      .header("FeideAuthorization", s"Bearer $feideToken")
+    val response = simpleHttpClient.send(request)
+
+    verify(folderReadService, times(0)).getAllResources(any, any)
+    verify(folderReadService, times(1)).getSingleFolder(eqTo(someId), any, any, any)
+    response.code.code should be(200)
+  }
+
+}
