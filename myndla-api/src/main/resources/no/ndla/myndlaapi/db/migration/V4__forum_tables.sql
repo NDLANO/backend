4e4c953fe3ccd2e03ac336e78edd8e0d909fb09b
Jonas Natten <jonas.natten@knowit.no>
2024-01-04 09:19:38 +0100

    myndla-api: Add deletion cascading to foreign keys
    This will make it so that deleting a topic deletes all connected posts.

--- myndla-api/src/main/resources/no/ndla/myndlaapi/db/migration/V4__forum_tables.sql
+++ myndla-api/src/main/resources/no/ndla/myndlaapi/db/migration/V4__forum_tables.sql
@@ -7,8 +7,8 @@ CREATE TABLE categories (
 CREATE TABLE topics (
     id BIGSERIAL PRIMARY KEY,
     title text,
-    category_id BIGINT REFERENCES categories(id),
-    owner_id BIGINT REFERENCES my_ndla_users(id),
+    category_id BIGINT REFERENCES categories(id) ON DELETE CASCADE,
+    owner_id BIGINT REFERENCES my_ndla_users(id) ON DELETE CASCADE,
     created timestamp NOT NULL DEFAULT now(),
     updated timestamp NOT NULL DEFAULT now()
 );
@@ -19,8 +19,8 @@ CREATE INDEX topics_owner_id_idx ON topics(owner_id);
 CREATE TABLE posts (
     id BIGSERIAL PRIMARY KEY,
     content text,
-    topic_id BIGINT REFERENCES topics(id),
-    owner_id BIGINT REFERENCES my_ndla_users(id),
+    topic_id BIGINT REFERENCES topics(id) ON DELETE CASCADE,
+    owner_id BIGINT REFERENCES my_ndla_users(id) ON DELETE CASCADE,
     created timestamp NOT NULL DEFAULT now(),
     updated timestamp NOT NULL DEFAULT now()
 );
@@ -31,7 +31,7 @@ CREATE INDEX posts_owner_id_idx ON posts(owner_id);
 CREATE TABLE category_follows (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES my_ndla_users(id),
-    category_id BIGINT REFERENCES categories(id)
+    category_id BIGINT REFERENCES categories(id) ON DELETE CASCADE
 );
 
 CREATE INDEX category_follows_user_id_idx ON category_follows(user_id);
@@ -40,7 +40,7 @@ CREATE INDEX category_follows_category_id_idx ON category_follows(category_id);
 CREATE TABLE topic_follows (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES my_ndla_users(id),
-    topic_id BIGINT REFERENCES topics(id)
+    topic_id BIGINT REFERENCES topics(id) ON DELETE CASCADE
 );
 
 CREATE INDEX topic_follows_user_id_idx ON topic_follows(user_id);
@@ -48,8 +48,8 @@ CREATE INDEX topic_follows_topic_id_idx ON topic_follows(topic_id);
 
 CREATE TABLE flags (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT REFERENCES my_ndla_users(id),
-    post_id BIGINT REFERENCES posts(id),
+    user_id BIGINT REFERENCES my_ndla_users(id) ON DELETE CASCADE,
+    post_id BIGINT REFERENCES posts(id) ON DELETE CASCADE,
     reason text,
     created timestamp NOT NULL DEFAULT now(),
     resolved timestamp NULL
@@ -60,9 +60,9 @@ CREATE INDEX flags_user_id_idx ON flags(user_id);
 
 CREATE TABLE notifications (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT REFERENCES my_ndla_users(id),
-    post_id BIGINT REFERENCES posts(id),
-    topic_id BIGINT REFERENCES topics(id),
+    user_id BIGINT REFERENCES my_ndla_users(id) ON DELETE CASCADE,
+    post_id BIGINT REFERENCES posts(id) ON DELETE CASCADE,
+    topic_id BIGINT REFERENCES topics(id) ON DELETE CASCADE,
     is_read BOOLEAN DEFAULT FALSE,
     notification_time timestamp NOT NULL DEFAULT now()
 );
