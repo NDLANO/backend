42c60efc7ca14513c9bb611490843304e57e8b6b
Jonas Natten <jonas.natten@knowit.no>
2024-01-25 13:31:01 +0100

    myndla: Add encoder/decoder for `ConfigMeta`
    Eithers are still weird with circe so we need this :^)

--- myndla/src/main/scala/no/ndla/myndla/model/api/config/ConfigMeta.scala
+++ myndla/src/main/scala/no/ndla/myndla/model/api/config/ConfigMeta.scala
@@ -7,9 +7,11 @@
 
 package no.ndla.myndla.model.api.config
 
-import no.ndla.common.model.NDLADate
+import io.circe.{Decoder, Encoder}
 import org.scalatra.swagger.annotations.ApiModelProperty
 import org.scalatra.swagger.runtime.annotations.ApiModel
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import no.ndla.common.model.NDLADate
 
 @ApiModel(description = "Describes configuration value.")
 case class ConfigMeta(
@@ -18,3 +20,11 @@ case class ConfigMeta(
     @ApiModelProperty(description = "Date of when configuration was last updated") updatedAt: NDLADate,
     @ApiModelProperty(description = "UserId of who last updated the configuration parameter.") updatedBy: String
 )
+
+object ConfigMeta {
+  import no.ndla.common.implicits._
+  implicit def eitherEnc: Encoder[Either[Boolean, List[String]]] = eitherEncoder
+  implicit def eitherDec: Decoder[Either[Boolean, List[String]]] = eitherDecoder
+  implicit def encoder: Encoder[ConfigMeta]                      = deriveEncoder
+  implicit def decoder: Decoder[ConfigMeta]                      = deriveDecoder
+}
