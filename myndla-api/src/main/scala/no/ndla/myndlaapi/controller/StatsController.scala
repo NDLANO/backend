bb033a319b3ac9de4d146e2182a137ce25b8b908
Jonas Natten <jonas.natten@knowit.no>
2024-02-15 09:49:05 +0100

    myndla-api: Add stats endpoint for resources
    This patch adds an endpoint which will return how many times a resource
    has been added to a folder.

--- myndla-api/src/main/scala/no/ndla/myndlaapi/controller/StatsController.scala
+++ myndla-api/src/main/scala/no/ndla/myndlaapi/controller/StatsController.scala
@@ -8,7 +8,7 @@
 package no.ndla.myndlaapi.controller
 
 import no.ndla.common.errors.NotFoundException
-import no.ndla.myndla.model.api.Stats
+import no.ndla.myndla.model.api.{SingleResourceStats, Stats}
 import no.ndla.myndla.service.FolderReadService
 import no.ndla.myndlaapi.Eff
 import no.ndla.network.tapir.NoNullJsonPrinter.jsonBody
@@ -22,8 +22,7 @@ import sttp.tapir.generic.auto._
 trait StatsController {
   this: FolderReadService with TapirErrorHelpers =>
   class StatsController extends Service[Eff] {
-    override val serviceName: String = "stats"
-
+    override val serviceName: String                   = "stats"
     override protected val prefix: EndpointInput[Unit] = "myndla-api" / "v1" / serviceName
 
     def getStats: ServerEndpoint[Any, Eff] = endpoint.get
@@ -37,9 +36,22 @@ trait StatsController {
           case None    => returnLeftError(NotFoundException("No stats found"))
         }
       }
+    private val pathResourceType = path[String]("resourceType").description("The type of the resource to look up")
+    private val pathResourceId   = path[String]("resourceId").description("ID of the resource to look up")
+
+    def getFolderResourceFavorites: ServerEndpoint[Any, Eff] = endpoint.get
+      .summary("Get folder resource favorites")
+      .description("Get folder resource favorites")
+      .in("favorites" / pathResourceType / pathResourceId)
+      .out(jsonBody[SingleResourceStats])
+      .errorOut(errorOutputsFor(404))
+      .serverLogicPure { case (resourceType, resourceId) =>
+        folderReadService.getFavouriteStatsForResource(resourceId, resourceType).handleErrorsOrOk
+      }
 
     override val endpoints: List[ServerEndpoint[Any, Eff]] = List(
-      getStats
+      getStats,
+      getFolderResourceFavorites
     )
   }
 }
