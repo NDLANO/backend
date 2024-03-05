c65c344f1d06c1d1674f96616e499180f32bcdda
Jonas Natten <jonas.natten@knowit.no>
2024-03-05 15:15:40 +0100

    article-api: Fix serialization for related content for articles

--- article-api/src/main/scala/no/ndla/articleapi/model/api/ArticleContent.scala
+++ article-api/src/main/scala/no/ndla/articleapi/model/api/ArticleContent.scala
@@ -8,6 +8,8 @@
 
 package no.ndla.articleapi.model.api
 
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
 import sttp.tapir.Schema.annotations.description
 
 @description("The content of the article in the specified language")
@@ -15,3 +17,8 @@ case class ArticleContentV2(
     @description("The html content") content: String,
     @description("ISO 639-1 code that represents the language used in the content") language: String
 )
+
+object ArticleContentV2 {
+  implicit val encoder: Encoder[ArticleContentV2] = deriveEncoder
+  implicit val decoder: Decoder[ArticleContentV2] = deriveDecoder
+}
