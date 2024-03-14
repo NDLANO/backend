49f5e50d966c3bc17f089c02e14b0548aae2d3a4
Jonas Natten <jonas.natten@knowit.no>
2024-03-14 10:28:17 +0100

    concept-api: Migrations cleanup!
    This patch removes the content of scala-migrations. Since they are already
    applied in the running environments and they all just modify existing
    data, we can remove them to not have to maintain them in the future
    (Like now when removing json4s).

--- concept-api/src/main/scala/no/ndla/conceptapi/db/migration/V5__MigrateStatusToQualityAssured.scala
+++ concept-api/src/main/scala/no/ndla/conceptapi/db/migration/V5__MigrateStatusToQualityAssured.scala
@@ -8,77 +8,7 @@
 package no.ndla.conceptapi.db.migration
 
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
-import org.json4s.*
-import org.json4s.JsonAST.{JObject, JString}
-import org.json4s.native.JsonMethods.{compact, parse, render}
-import org.postgresql.util.PGobject
-import scalikejdbc.{DB, DBSession, _}
 
 class V5__MigrateStatusToQualityAssured extends BaseJavaMigration {
-  implicit val formats: DefaultFormats.type = DefaultFormats
-
-  override def migrate(context: Context): Unit = DB(context.getConnection)
-    .autoClose(false)
-    .withinTx { implicit session =>
-      migrateConcepts
-    }
-
-  def migrateConcepts(implicit session: DBSession): Unit = {
-    val count        = countAllConcepts.get
-    var numPagesLeft = (count / 1000) + 1
-    var offset       = 0L
-
-    while (numPagesLeft > 0) {
-      allConcepts(offset * 1000).foreach { case (id, document) =>
-        updateConcept(convertToNewConcept(document), id)
-      }
-      numPagesLeft -= 1
-      offset += 1
-    }
-  }
-
-  def countAllConcepts(implicit session: DBSession): Option[Long] = {
-    sql"select count(*) from conceptdata where document is not NULL"
-      .map(rs => rs.long("count"))
-      .single()
-  }
-
-  def allConcepts(offset: Long)(implicit session: DBSession): Seq[(Long, String)] = {
-    sql"select id, document from conceptdata where document is not null order by id limit 1000 offset $offset"
-      .map(rs => {
-        (rs.long("id"), rs.string("document"))
-      })
-      .list()
-  }
-
-  def updateConcept(document: String, id: Long)(implicit session: DBSession): Int = {
-    val dataObject = new PGobject()
-    dataObject.setType("jsonb")
-    dataObject.setValue(document)
-
-    sql"update conceptdata set document = $dataObject where id = $id"
-      .update()
-  }
-
-  def renameStatus(status: String): String = {
-    val mapping = Map(
-      "QUEUED_FOR_PUBLISHING" -> "QUALITY_ASSURED"
-    )
-    mapping.getOrElse(status, status)
-  }
-
-  def convertToNewConcept(document: String): String = {
-    val concept = parse(document)
-
-    val newConcept = concept.mapField {
-      case ("status", status: JObject) =>
-        "status" -> status.mapField {
-          case ("current", current: JString) =>
-            "current" -> JString(renameStatus(current.values))
-          case x => x
-        }
-      case x => x
-    }
-    compact(render(newConcept))
-  }
+  override def migrate(context: Context): Unit = {}
 }
