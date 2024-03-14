3fcfe400deda169b533e3e0a6ad23f3e35060024
Jonas Natten <jonas.natten@knowit.no>
2024-03-14 14:46:18 +0100

    learningpath-api: Migrations cleanup!
    This patch removes the content of scala-migrations. Since they are already
    applied in the running environments and they all just modify existing
    data, we can remove them to not have to maintain them in the future
    (Like now when removing json4s).

--- learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V6__UpdateDuration.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V6__UpdateDuration.scala
@@ -8,51 +8,7 @@
 package no.ndla.learningpathapi.db.migration
 
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
-import org.json4s._
-import org.json4s.native.JsonMethods.{compact, parse, render}
-import org.postgresql.util.PGobject
-import scalikejdbc.{DB, DBSession, _}
 
 class V6__UpdateDuration extends BaseJavaMigration {
-
-  override def migrate(context: Context): Unit = DB(context.getConnection)
-    .autoClose(false)
-    .withinTx { implicit session =>
-      allLearningPaths
-        .map { case (id, document) =>
-          (id, updateDuration(document))
-        }
-        .foreach { case (id, document) =>
-          update(id, document)
-        }
-    }
-
-  def allLearningPaths(implicit session: DBSession): Seq[(Long, String)] = {
-    sql"select id, document from learningpaths"
-      .map(rs => {
-        (rs.long("id"), rs.string("document"))
-      })
-      .list()
-  }
-
-  def updateDuration(document: String): String = {
-    val oldLearningPath = parse(document)
-
-    val newLearningPath = oldLearningPath.mapField {
-      case ("duration", JInt(duration)) if duration <= 0 =>
-        "duration" -> JInt(1)
-      case x => x
-    }
-    compact(render(newLearningPath))
-  }
-
-  def update(id: Long, document: String)(implicit session: DBSession): Int = {
-    val dataObject = new PGobject()
-    dataObject.setType("jsonb")
-    dataObject.setValue(document)
-
-    sql"update learningpaths set document = $dataObject where id = ${id}"
-      .update()
-  }
-
+  override def migrate(context: Context): Unit = {}
 }
