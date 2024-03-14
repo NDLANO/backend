42284035803840879d7b754254d2b736bff08025
Jonas Natten <jonas.natten@knowit.no>
2024-03-14 14:46:18 +0100

    learningpath-api: Migrations cleanup!
    This patch removes the content of scala-migrations. Since they are already
    applied in the running environments and they all just modify existing
    data, we can remove them to not have to maintain them in the future
    (Like now when removing json4s).

--- learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V4__ConvertStatusNotListedToPrivate.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V4__ConvertStatusNotListedToPrivate.scala
@@ -9,44 +9,7 @@
 package no.ndla.learningpathapi.db.migration
 
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
-import org.json4s._
-import org.json4s.native.JsonMethods._
-import org.postgresql.util.PGobject
-import scalikejdbc._
 
 class V4__ConvertStatusNotListedToPrivate extends BaseJavaMigration {
-
-  implicit val formats: Formats = org.json4s.DefaultFormats
-
-  override def migrate(context: Context): Unit = DB(context.getConnection)
-    .autoClose(false)
-    .withinTx { implicit session =>
-      allLearningPaths.map(convertLearningPathStatus).foreach(update)
-    }
-
-  def allLearningPaths(implicit session: DBSession): List[V4_DBLearningPath] = {
-    sql"select id, document from learningpaths where document ->> 'status' = 'NOT_LISTED'"
-      .map(rs => V4_DBLearningPath(rs.long("id"), rs.string("document")))
-      .list()
-  }
-
-  def convertLearningPathStatus(learningPath: V4_DBLearningPath): V4_DBLearningPath = {
-    val oldDocument = parse(learningPath.document)
-    val updatedDocument = oldDocument mapField {
-      case ("status", JString(oldStatus @ _)) => ("status", JString("PRIVATE"))
-      case x                                  => x
-    }
-    learningPath.copy(document = compact(render(updatedDocument)))
-  }
-
-  def update(learningPath: V4_DBLearningPath)(implicit session: DBSession): Int = {
-    val dataObject = new PGobject()
-    dataObject.setType("jsonb")
-    dataObject.setValue(learningPath.document)
-
-    sql"update learningpaths set document = $dataObject where id = ${learningPath.id}"
-      .update()
-  }
+  override def migrate(context: Context): Unit = {}
 }
-
-case class V4_DBLearningPath(id: Long, document: String)
--- learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V7__MovePublishedExternToUnlisted.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V7__MovePublishedExternToUnlisted.scala
@@ -8,58 +8,7 @@
 package no.ndla.learningpathapi.db.migration
 
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
-import org.json4s._
-import org.json4s.native.JsonMethods.{compact, parse, render}
-import org.postgresql.util.PGobject
-import scalikejdbc.{DB, DBSession, _}
 
 class V7__MovePublishedExternToUnlisted extends BaseJavaMigration {
-
-  implicit val formats: DefaultFormats.type = DefaultFormats
-
-  override def migrate(context: Context): Unit = DB(context.getConnection)
-    .autoClose(false)
-    .withinTx { implicit session =>
-      allLearningPaths
-        .map { case (id, document) =>
-          (id, updateStatus(document))
-        }
-        .foreach { case (id, document) =>
-          update(id, document)
-        }
-    }
-
-  def allLearningPaths(implicit session: DBSession): Seq[(Long, String)] = {
-    sql"select id, document from learningpaths"
-      .map(rs => {
-        (rs.long("id"), rs.string("document"))
-      })
-      .list()
-  }
-
-  def updateStatus(document: String): String = {
-    val oldLearningPath = parse(document)
-
-    val newLearningPath = (oldLearningPath \ "verificationStatus").extractOpt[String] match {
-      case Some(verStat) if verStat == "EXTERNAL" =>
-        oldLearningPath.mapField {
-          case ("status", JString(status)) if status == "PUBLISHED" =>
-            "status" -> JString("UNLISTED")
-          case x => x
-        }
-      case _ => oldLearningPath
-    }
-
-    compact(render(newLearningPath))
-  }
-
-  def update(id: Long, document: String)(implicit session: DBSession): Int = {
-    val dataObject = new PGobject()
-    dataObject.setType("jsonb")
-    dataObject.setValue(document)
-
-    sql"update learningpaths set document = $dataObject where id = ${id}"
-      .update()
-  }
-
+  override def migrate(context: Context): Unit = {}
 }
