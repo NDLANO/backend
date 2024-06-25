569fa7431f170caf03ed18b1f17b80e4411e51a2
Gaute Rauboti <33912044+rauboti@users.noreply.github.com>
2024-06-25 13:11:45 +0200

    Update myndla-api/src/main/resources/no/ndla/myndlaapi/db/migration/V13__post_upvote_table.sql
    Co-authored-by: Gunnar Velle <gunnar.velle@gmail.com>

--- myndla-api/src/main/resources/no/ndla/myndlaapi/db/migration/V13__post_upvote_table.sql
+++ myndla-api/src/main/resources/no/ndla/myndlaapi/db/migration/V13__post_upvote_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE post_upvote (
+CREATE TABLE IF NOT EXISTS post_upvote (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES my_ndla_users(id) ON DELETE CASCADE,
     post_id BIGINT REFERENCES posts(id) ON DELETE CASCADE
