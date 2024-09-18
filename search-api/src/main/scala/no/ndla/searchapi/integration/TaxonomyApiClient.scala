a5ee69fbd0ef8383d2d37648baa1399a9ba82ab9
Jonas Natten <jonas.natten@knowit.no>
2024-09-18 10:30:35 +0200

    yay

--- search-api/src/main/scala/no/ndla/searchapi/integration/TaxonomyApiClient.scala
+++ search-api/src/main/scala/no/ndla/searchapi/integration/TaxonomyApiClient.scala
@@ -132,8 +132,7 @@ trait TaxonomyApiClient {
         val numPages  = Math.ceil(firstPage.totalCount.toDouble / pageSize.toDouble).toInt
         val pageRange = 1 to numPages
 
-
-        val numThreads = props.TaxonomyFetchingThreads.getOrElse(Math.max(20, numPages))
+        val numThreads = 1
         implicit val executionContext: ExecutionContextExecutorService =
           ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(numThreads))
 
