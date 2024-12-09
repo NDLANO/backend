c9b94e1c845ff663b2732e1101f704317ab05044
Jonas Natten <jonas.natten@knowit.no>
2024-12-09 12:06:44 +0100

    learningpath-api: Add `external` embed url to learningsteps

--- common/src/main/scala/no/ndla/common/model/domain/learningpath/EmbedUrl.scala
+++ common/src/main/scala/no/ndla/common/model/domain/learningpath/EmbedUrl.scala
@@ -29,9 +29,10 @@ sealed abstract class EmbedType(override val entryName: String) extends EnumEntr
 }
 
 object EmbedType extends Enum[EmbedType] with CirceEnum[EmbedType] {
-  case object OEmbed extends EmbedType("oembed")
-  case object LTI    extends EmbedType("lti")
-  case object IFrame extends EmbedType("iframe")
+  case object OEmbed   extends EmbedType("oembed")
+  case object LTI      extends EmbedType("lti")
+  case object IFrame   extends EmbedType("iframe")
+  case object External extends EmbedType("external")
 
   def valueOf(s: String): Option[EmbedType]  = EmbedType.values.find(_.entryName == s)
   def valueOfOrDefault(s: String): EmbedType = valueOf(s).getOrElse(EmbedType.OEmbed)
