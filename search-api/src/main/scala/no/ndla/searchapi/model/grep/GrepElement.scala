4df6953ecf63e7b636018390eef92697dd13e521
Jonas Natten <jonas.natten@knowit.no>
2024-12-11 15:15:20 +0100

    search-api: Add grep læreplan to indexed search

--- search-api/src/main/scala/no/ndla/searchapi/model/grep/GrepElement.scala
+++ search-api/src/main/scala/no/ndla/searchapi/model/grep/GrepElement.scala
@@ -7,12 +7,56 @@
 
 package no.ndla.searchapi.model.grep
 
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 
-case class GrepElement(kode: String, tittel: Seq[GrepTitle])
+sealed trait GrepElement {
+  val kode: String
+  val tittel: Seq[GrepTitle]
+}
 
-object GrepElement {
-  implicit val encoder: Encoder[GrepElement] = deriveEncoder
-  implicit val decoder: Decoder[GrepElement] = deriveDecoder
+sealed trait BelongsToLaerePlan {
+  val tilhoerer_laereplan: BelongsToObj
+}
+
+case class GrepKjerneelement(kode: String, tittel: Seq[GrepTitle], tilhoerer_laereplan: BelongsToObj)
+    extends GrepElement
+    with BelongsToLaerePlan
+object GrepKjerneelement {
+  implicit val encoder: Encoder[GrepKjerneelement] = deriveEncoder
+  implicit val decoder: Decoder[GrepKjerneelement] = deriveDecoder
+}
+
+case class BelongsToObj(kode: String)
+object BelongsToObj {
+  implicit val encoder: Encoder[BelongsToObj] = deriveEncoder
+  implicit val decoder: Decoder[BelongsToObj] = deriveDecoder
+}
+
+case class GrepKompetansemaal(kode: String, tittel: Seq[GrepTitle], tilhoerer_laereplan: BelongsToObj)
+    extends GrepElement
+    with BelongsToLaerePlan
+object GrepKompetansemaal {
+  implicit val encoder: Encoder[GrepKompetansemaal] = deriveEncoder
+  implicit val decoder: Decoder[GrepKompetansemaal] = deriveDecoder
+}
+
+case class GrepKompetansemaalSett(kode: String, tittel: Seq[GrepTitle], tilhoerer_laereplan: BelongsToObj)
+    extends GrepElement
+    with BelongsToLaerePlan
+object GrepKompetansemaalSett {
+  implicit val encoder: Encoder[GrepKompetansemaalSett] = deriveEncoder
+  implicit val decoder: Decoder[GrepKompetansemaalSett] = deriveDecoder
+}
+
+case class GrepLaererplan(kode: String, tittel: Seq[GrepTitle]) extends GrepElement
+object GrepLaererplan {
+  implicit val encoder: Encoder[GrepLaererplan] = deriveEncoder
+  implicit val decoder: Decoder[GrepLaererplan] = deriveDecoder
+}
+
+case class GrepTverrfagligTema(kode: String, tittel: Seq[GrepTitle]) extends GrepElement
+object GrepTverrfagligTema {
+  implicit val encoder: Encoder[GrepTverrfagligTema] = deriveEncoder
+  implicit val decoder: Decoder[GrepTverrfagligTema] = deriveDecoder
 }
