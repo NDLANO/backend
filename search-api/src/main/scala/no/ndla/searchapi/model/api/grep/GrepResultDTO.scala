be43cb8cc56b938bf5125b239a3fd44bc3b15c7b
Jonas Natten <jonas.natten@knowit.no>
2025-01-02 15:53:29 +0100

    yo

--- search-api/src/main/scala/no/ndla/searchapi/model/api/grep/GrepResultDTO.scala
+++ search-api/src/main/scala/no/ndla/searchapi/model/api/grep/GrepResultDTO.scala
@@ -8,19 +8,31 @@
 
 package no.ndla.searchapi.model.api.grep
 
-import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import cats.implicits.*
+import io.circe.generic.auto.*
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.syntax.*
 import io.circe.{Decoder, Encoder}
 import no.ndla.searchapi.model.api.TitleDTO
 import sttp.tapir.Schema.annotations.description
 
 @description("Information about a single grep search result entry")
-case class GrepResultDTO(
-    @description("The grep code") code: String,
-    @description("The greps title") title: TitleDTO,
-    @description("The grep laereplan") laereplanCode: Option[String]
-)
+sealed trait GrepResultDTO {
+  @description("The grep code") val code: String
+  @description("The greps title") val title: TitleDTO
+}
 
 object GrepResultDTO {
-  implicit val encoder: Encoder[GrepResultDTO] = deriveEncoder
-  implicit val decoder: Decoder[GrepResultDTO] = deriveDecoder
+  implicit val encoder: Encoder[GrepResultDTO] = Encoder.instance { case x: GrepKjerneelementDTO =>
+    x.asJson
+  }
+  implicit val decoder: Decoder[GrepResultDTO] = List[Decoder[GrepResultDTO]](
+    Decoder[GrepKjerneelementDTO].widen
+  ).reduceLeft(_ or _)
 }
+
+case class GrepKjerneelementDTO(
+    code: String,
+    title: TitleDTO,
+    laereplanCode: Option[String]
+) extends GrepResultDTO
