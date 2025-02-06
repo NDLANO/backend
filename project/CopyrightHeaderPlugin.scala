54c73e1d7c738ff4781cc63037deb48c57a45c34
Jonas Natten <jonas.natten@knowit.no>
2025-02-06 14:20:40 +0100

    Add `copyrightCheck` task for CI

--- project/CopyrightHeaderPlugin.scala
+++ project/CopyrightHeaderPlugin.scala
@@ -6,7 +6,7 @@
  *
  */
 
-import CopyrightHeaderPlugin.autoImport.copyrightGenerate
+import CopyrightHeaderPlugin.autoImport.{copyrightGenerate, copyrightCheck}
 import sbt.*
 import sbt.Keys.*
 import sbt.AutoPlugin
@@ -20,6 +20,7 @@ object CopyrightHeaderPlugin extends AutoPlugin {
 
   object autoImport {
     val copyrightGenerate: TaskKey[Unit] = taskKey[Unit]("Generate copyright for scala files")
+    val copyrightCheck: TaskKey[Unit]    = taskKey[Unit]("Check copyright for scala files")
   }
 
   def copyrightTemplate(year: String, submodule: Option[String]): String = {
@@ -52,7 +53,7 @@ object CopyrightHeaderPlugin extends AutoPlugin {
 
   private val currentYear = java.time.Year.now.getValue.toString
 
-  def validateOrFixFile(submodule: Option[String], file: File): Unit = {
+  def validateOrFixFile(submodule: Option[String], file: File, skipWrite: Boolean = false): Boolean = {
     val lines             = readLines(file)
     val copyrightLocation = locateCopyright(lines)
     val output = copyrightLocation match {
@@ -67,19 +68,30 @@ object CopyrightHeaderPlugin extends AutoPlugin {
         val cop = copyrightTemplate(currentYear, submodule)
         cop.split("\n").toList ++ lines
     }
-    if (output != lines) {
+    val mismatch = output != lines
+    if (mismatch) {
       println(s"${file.getAbsolutePath}")
-      Files.write(
-        file.toPath,
-        output.asJava,
-        StandardOpenOption.CREATE,
-        StandardOpenOption.TRUNCATE_EXISTING
-      )
+      if (!skipWrite)
+        Files.write(file.toPath, output.asJava, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
     }
+    mismatch
   }
 
   override lazy val projectSettings: Seq[Setting[?]] = Seq(
     Compile / compile := (Compile / compile dependsOn copyrightGenerate).value,
+    copyrightCheck := {
+      val appName                = name.value
+      val sourceFiles: Seq[File] = (Compile / sources).value
+      val testFiles: Seq[File]   = (Test / sources).value
+      val allFiles               = sourceFiles ++ testFiles
+      val filteredPath           = s"/$appName/target/scala-2.13/src_managed"
+      val filteredFiles          = allFiles.filterNot(_.getAbsolutePath.contains(filteredPath))
+
+      val gotError = filteredFiles.foldLeft(false) { case (foundError, file) =>
+        validateOrFixFile(appName.some, file, skipWrite = true) || foundError
+      }
+      if (gotError) throw new RuntimeException("Some files have incorrect copyright headers...")
+    },
     copyrightGenerate := {
       val appName                = name.value
       val sourceFiles: Seq[File] = (Compile / sources).value
