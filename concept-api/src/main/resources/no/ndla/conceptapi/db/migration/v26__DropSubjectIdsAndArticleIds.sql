bcf4e561f01a50fd1e7a4d2c3964c07c3c1ca40a
Gunnar Velle <gunnar.velle@gmail.com>
2025-02-10 09:18:39 +0100

    Fix migration

--- concept-api/src/main/resources/no/ndla/conceptapi/db/migration/v26__DropSubjectIdsAndArticleIds.sql
+++
@@ -1,5 +0,0 @@
-update conceptdata set document = document - 'articleIds'
-update conceptdata set document = document - 'subjectIds'
-
-update publishedconceptdata set document = document - 'articleIds'
-update publishedconceptdata set document = document - 'subjectIds'
