8259fe981185f9a78358f6c184492bab5ae59aba
Gunnar Velle <gunnar.velle@gmail.com>
2025-02-21 10:42:02 +0100

    Converts norgesfilm-urls for new format

---
+++ draft-api/src/main/scala/no/ndla/draftapi/db/migration/V69__ConvertNorgesfilmUrls.scala
@@ -0,0 +1,28 @@
+/*
+ * Part of NDLA draft-api
+ * Copyright (C) 2025 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.draftapi.db.migration
+
+import no.ndla.draftapi.db.HtmlMigration
+import org.jsoup.nodes.Element
+
+class V69__ConvertNorgesfilmUrls extends HtmlMigration {
+  override val convertVisualElement: Boolean = true
+  override def convertHtml(doc: Element, language: String): Element = {
+    doc
+      .select("ndlaembed[data-resource='iframe']")
+      .forEach(embed => {
+        val url = embed.attr("data-url")
+        if (url.contains("ndla.filmiundervisning.no/film/ndlafilm.aspx?")) {
+          embed.attr("data-url", url.replace("/ndlafilm.aspx?", "/")): Unit
+        }
+      })
+    doc
+  }
+
+}
---
+++ draft-api/src/test/scala/no/ndla/draftapi/db/migration/V69__ConvertNorgesfilmUrlsTest.scala
@@ -0,0 +1,23 @@
+/*
+ * Part of NDLA draft-api
+ * Copyright (C) 2025 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.draftapi.db.migration
+
+import no.ndla.draftapi.{TestEnvironment, UnitSuite}
+
+class V69__ConvertNorgesfilmUrlsTest extends UnitSuite with TestEnvironment {
+  test("That norgesfilm urls looses ndlafilm.aspx") {
+    val migration = new V69__ConvertNorgesfilmUrls
+    val oldArticle =
+      """<section><ndlaembed data-resource="iframe" data-type="iframe" data-url="https://ndla.filmiundervisning.no/film/ndlafilm.aspx?filmId=13074" data-width="700" data-height="300"></ndlaembed></section>"""
+    val newArticle =
+      """<section><ndlaembed data-resource="iframe" data-type="iframe" data-url="https://ndla.filmiundervisning.no/film/filmId=13074" data-width="700" data-height="300"></ndlaembed></section>"""
+
+    migration.convertContent(oldArticle, "nb") should be(newArticle)
+  }
+}
