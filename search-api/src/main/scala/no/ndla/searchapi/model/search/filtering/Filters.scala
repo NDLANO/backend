befadb0dbabdb39c75b964124cdccc61dc5a5bb4
Jonas Natten <jonas.natten@knowit.no>
2025-02-28 06:51:18 +0100

    hmm

--- search-api/src/main/scala/no/ndla/searchapi/model/search/filtering/Filters.scala
+++ search-api/src/main/scala/no/ndla/searchapi/model/search/filtering/Filters.scala
@@ -1,3 +1,10 @@
+/*
+ * Part of NDLA search-api
+ * Copyright (C) 2025 NDLA
+ *
+ * See LICENSE
+ *
+ */
 package no.ndla.searchapi.model.search.filtering
 
 import com.sksamuel.elastic4s.ElasticDsl.*
@@ -14,6 +21,14 @@ case class TermFilter[T](path: String, filterValue: T) extends FilterBase {
 }
 case class TermsFilter[T](path: String, filterValues: List[T]) extends FilterBase {
   override def toE4s: Query = termsQuery(path, filterValues)
+}
+case class BoolQuery(and: List[FilterBase], or: List[FilterBase], not: List[FilterBase]) {
+  def toE4s: Query = {
+    val andQueries = and.map(_.toE4s)
+    val orQueries  = or.map(_.toE4s)
+    val notQueries = not.map(_.toE4s)
+    boolQuery().must(andQueries).should(orQueries).not(notQueries)
+  }
 }
 
 case class Filters(filters: List[FilterBase]) {
