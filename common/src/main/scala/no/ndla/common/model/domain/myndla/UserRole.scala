686b013c70f49bd7e0cecb8a4734a0b0b438e0ce
Jonas Natten <jonas.natten@knowit.no>
2025-03-19 08:39:51 +0100

    myndla-api: Add/fix schema for `UserRole`

--- common/src/main/scala/no/ndla/common/model/domain/myndla/UserRole.scala
+++ common/src/main/scala/no/ndla/common/model/domain/myndla/UserRole.scala
@@ -12,6 +12,8 @@ import enumeratum.*
 import com.scalatsi.{TSNamedType, TSType}
 import com.scalatsi.TypescriptType.{TSLiteralString, TSUnion}
 import enumeratum.{CirceEnum, EnumEntry}
+import sttp.tapir.Schema
+import sttp.tapir.codec.enumeratum.*
 
 sealed abstract class UserRole(override val entryName: String) extends EnumEntry {
   override def toString: String = entryName
@@ -24,6 +26,7 @@ object UserRole extends Enum[UserRole] with CirceEnum[UserRole] {
 
   def all: Seq[String]                     = UserRole.values.map(_.entryName)
   def valueOf(s: String): Option[UserRole] = UserRole.withNameOption(s)
+  implicit val schema: Schema[UserRole]    = schemaForEnumEntry[UserRole]
 
   implicit val availability: TSNamedType[UserRole] =
     TSType.alias[UserRole]("UserRole", TSUnion(values.map(e => TSLiteralString(e.entryName))))
