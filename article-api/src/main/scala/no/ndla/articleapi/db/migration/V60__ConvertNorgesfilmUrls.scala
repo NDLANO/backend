f8b747d91d00098103a71f23f988ab006d43f3ba
Gunnar Velle <gunnar.velle@gmail.com>
2025-06-10 15:31:04 +0200

    Rename tests to fit with master

--- article-api/src/main/scala/no/ndla/articleapi/db/migration/V60__ConvertNorgesfilmUrls.scala
+++ article-api/src/main/scala/no/ndla/articleapi/db/migration/V60__ConvertNorgesfilmUrls.scala
@@ -1,17 +1,17 @@
 /*
- * Part of NDLA draft-api
+ * Part of NDLA article-api
  * Copyright (C) 2025 NDLA
  *
  * See LICENSE
  *
  */
 
-package no.ndla.draftapi.db.migration
+package no.ndla.articleapi.db.migration
 
-import no.ndla.draftapi.db.HtmlMigration
+import no.ndla.articleapi.db.HtmlMigration
 import org.jsoup.nodes.Element
 
-class V69__ConvertNorgesfilmUrls extends HtmlMigration {
+class V60__ConvertNorgesfilmUrls extends HtmlMigration {
   override val convertVisualElement: Boolean = true
   override def convertHtml(doc: Element, language: String): Element = {
     doc
@@ -24,5 +24,4 @@ class V69__ConvertNorgesfilmUrls extends HtmlMigration {
       })
     doc
   }
-
 }
