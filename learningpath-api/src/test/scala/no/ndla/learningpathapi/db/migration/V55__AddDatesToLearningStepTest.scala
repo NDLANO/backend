eaa56ad2958f21d7034865a381a1fcfdb8630ce9
Gunnar Velle <gunnar.velle@gmail.com>
2025-09-02 13:56:30 +0200

    Set created and lastupdated from learningpath

---
+++ learningpath-api/src/test/scala/no/ndla/learningpathapi/db/migration/V55__AddDatesToLearningStepTest.scala
@@ -0,0 +1,57 @@
+/*
+ * Part of NDLA learningpath-api
+ * Copyright (C) 2025 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.learningpathapi.db.migration
+
+import no.ndla.common.CirceUtil
+import no.ndla.learningpathapi.db.util.{LpDocumentRow, StepDocumentRow}
+import no.ndla.learningpathapi.{TestEnvironment, UnitSuite}
+
+class V55__AddDatesToLearningStepTest extends UnitSuite with TestEnvironment {
+
+  test("that license is moved to copyright with empty contributors") {
+    val migration    = new V55__AddDatesToLearningStep
+    val learningpath =
+      """
+        |{
+        |  "license": "CC BY-NC-SA 4.0",
+        |  "title": "Test Step",
+        |  "description": "This is a test step.",
+        |  "created": "2024-01-01T00:00:00Z"
+        |}
+        |""".stripMargin
+    val stepWithoutDates =
+      """
+        |{
+        |  "title": "Test Step",
+        |  "description": "This is a test step."
+        |}
+        |""".stripMargin
+
+    val expectedDocument =
+      """
+        |{
+        |  "title": "Test Step",
+        |  "description": "This is a test step.",
+        |  "created": "2024-01-01T00:00:00Z",
+        |  "lastUpdated": "2024-01-01T00:00:00Z"
+        |}
+        |""".stripMargin
+
+    val expectedConverted = CirceUtil.unsafeParse(expectedDocument)
+
+    val lpDoc                      = LpDocumentRow(1, learningpath)
+    val stepDoc1                   = StepDocumentRow(1, stepWithoutDates)
+    val (resultLpDoc, resultSteps) = migration.convertPathAndSteps(lpDoc, List(stepDoc1))
+
+    resultLpDoc should be(lpDoc)
+    val result1Json = CirceUtil.unsafeParse(resultSteps.head.learningStepDocument)
+
+    result1Json should be(expectedConverted)
+  }
+}
