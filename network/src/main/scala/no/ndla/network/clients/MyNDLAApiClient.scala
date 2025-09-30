1653b4cfdbe77ebf589d3f5d590d7ea5a6f234f9
Gunnar Velle <gunnar.velle@gmail.com>
2025-09-30 12:51:19 +0200

    Remove LearningpathWriteRestricted

--- network/src/main/scala/no/ndla/network/clients/MyNDLAApiClient.scala
+++ network/src/main/scala/no/ndla/network/clients/MyNDLAApiClient.scala
@@ -10,15 +10,12 @@ package no.ndla.network.clients
 
 import no.ndla.common.configuration.BaseProps
 import no.ndla.common.model.api.{MyNDLABundleDTO, SingleResourceStatsDTO}
-import no.ndla.common.model.api.config.ConfigMetaRestrictedDTO
 import no.ndla.common.model.domain.ResourceType
-import no.ndla.common.model.domain.config.ConfigKey
 import no.ndla.common.model.api.myndla as api
 import no.ndla.network.NdlaClient
-import no.ndla.network.model.NdlaRequest
 import sttp.client3.quick.*
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Success, Try}
 
 class MyNDLAApiClient(using props: BaseProps, ndlaClient: NdlaClient) {
   private val statsEndpoint = s"http://${props.MyNDLAApiHost}/myndla-api/v1/stats"
@@ -30,16 +27,7 @@ class MyNDLAApiClient(using props: BaseProps, ndlaClient: NdlaClient) {
   }
 
   def isWriteRestricted: Try[Boolean] = {
-    doRequest(
-      quickRequest.get(
-        uri"http://${props.MyNDLAApiHost}/myndla-api/v1/config/${ConfigKey.LearningpathWriteRestricted.entryName}"
-      )
-    ).flatMap(
-      _.value match {
-        case Left(value) => Success(value)
-        case _           => Failure(new RuntimeException("Expected boolean, got list"))
-      }
-    )
+    Success(false)
   }
 
   def getStatsFor(id: String, resourceTypes: List[ResourceType]): Try[List[SingleResourceStatsDTO]] = {
@@ -53,9 +41,5 @@ class MyNDLAApiClient(using props: BaseProps, ndlaClient: NdlaClient) {
     val req = quickRequest.get(url)
     val res = ndlaClient.fetch[Map[String, Map[String, Long]]](req)
     res.map(favMap => MyNDLABundleDTO(favMap))
-  }
-
-  private def doRequest(httpRequest: NdlaRequest): Try[ConfigMetaRestrictedDTO] = {
-    ndlaClient.fetchWithForwardedAuth[ConfigMetaRestrictedDTO](httpRequest, None)
   }
 }
