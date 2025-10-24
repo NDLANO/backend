4d088a27f72f3139c4a2a59e626357034b8665a6
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-10-24 10:59:24 +0200

    Add migration for existing `ImageFileData`s

---
+++ image-api/src/main/scala/no/ndla/imageapi/db/migration/V22__AddImageVariants.scala
@@ -0,0 +1,14 @@
+package no.ndla.imageapi.db.migration
+
+import io.circe.{Json, parser}
+import no.ndla.database.DocumentMigration
+
+class V22__AddImageVariants extends DocumentMigration {
+  override val tableName: String  = "imagefiledata"
+  override val columnName: String = "metadata"
+
+  override def convertColumn(value: String): String = {
+    val oldDocument = parser.parse(value).toTry.get
+    oldDocument.mapObject(_.add("variants", Json.arr())).noSpaces
+  }
+}
