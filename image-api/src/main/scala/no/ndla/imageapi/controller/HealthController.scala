4581dcdb2be7f107cd753cc153bd6612b0ed8532
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2025-11-20 11:00:59 +0100

    fix(image-api): make health check pass if database is empty

--- image-api/src/main/scala/no/ndla/imageapi/controller/HealthController.scala
+++ image-api/src/main/scala/no/ndla/imageapi/controller/HealthController.scala
@@ -8,10 +8,13 @@
 
 package no.ndla.imageapi.controller
 
+import no.ndla.imageapi.model.domain.ImageMetaInformation
 import no.ndla.imageapi.repository.ImageRepository
 import no.ndla.imageapi.service.ImageStorageService
 import no.ndla.network.clients.MyNDLAApiClient
 import no.ndla.network.tapir.{ErrorHandling, ErrorHelpers, TapirHealthController}
+
+import scala.util.{Failure, Success}
 
 class HealthController(using
     imageStorageService: ImageStorageService,
@@ -20,21 +23,20 @@ class HealthController(using
     errorHelpers: ErrorHelpers,
     errorHandling: ErrorHandling,
 ) extends TapirHealthController {
-
   override def checkReadiness(): Either[String, String] = {
-    val maybeHealthy = for {
-      imageMeta <- imageRepository
-        .getRandomImage()
-        .recover { ex =>
-          logger.error("Failed to fetch random image in health check", ex)
-          None
-        }
-        .toOption
-        .flatten
+    val maybeHealth = for {
+      imageMeta <- randomImage
       imageFile <- imageMeta.images.headOption
-      healthy   <- Option.when(imageStorageService.objectExists(imageFile.fileName))("Healthy")
+      healthy    = Either.cond(imageStorageService.objectExists(imageFile.fileName), "Healthy", "Internal server error")
     } yield healthy
 
-    maybeHealthy.toRight("Internal server error")
+    maybeHealth.getOrElse(Right("Healthy"))
+  }
+
+  private def randomImage: Option[ImageMetaInformation] = imageRepository.getRandomImage() match {
+    case Success(meta) => meta
+    case Failure(ex)   =>
+      logger.error("Failed to fetch random image in health check", ex)
+      None
   }
 }
