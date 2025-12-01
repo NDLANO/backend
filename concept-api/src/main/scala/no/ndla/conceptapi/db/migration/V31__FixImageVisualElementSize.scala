75764128aeedc47a01ed3df53892205a54143cd9
Gunnar Velle <gunnar.velle@gmail.com>
2025-12-01 10:32:45 +0100

    Add error handling and use toTry.get

--- concept-api/src/main/scala/no/ndla/conceptapi/db/migration/V31__FixImageVisualElementSize.scala
+++ concept-api/src/main/scala/no/ndla/conceptapi/db/migration/V31__FixImageVisualElementSize.scala
@@ -10,39 +10,40 @@ package no.ndla.conceptapi.db.migration
 
 import io.circe.parser
 import no.ndla.database.DocumentMigration
+import scala.util.Try
 
 class V31__FixImageVisualElementSize extends DocumentMigration {
   override val tableName  = "conceptdata"
   override val columnName = "document"
 
   override def convertColumn(value: String): String = {
-    parser
-      .parse(value)
-      .toOption
-      .flatMap { doc =>
-        val visualElements = doc.hcursor.downField("visualElement")
-        if (visualElements.succeeded) {
-          val updated = visualElements.withFocus(
-            _.mapArray(
-              _.map { visualElements =>
-                visualElements
-                  .hcursor
-                  .downField("visualElement")
-                  .withFocus(visualElement =>
-                    visualElement
-                      .asString
-                      .map(_.replace("data-size=\"fullbredde\"", "data-size=\"full\""))
-                      .map(io.circe.Json.fromString)
-                      .getOrElse(visualElement)
-                  )
-                  .top
-                  .getOrElse(visualElements)
-              }
-            )
+    Try {
+      val doc            = parser.parse(value).toTry.get
+      val visualElements = doc.hcursor.downField("visualElement")
+      if (visualElements.succeeded) {
+        val updated = visualElements.withFocus(
+          _.mapArray(
+            _.map { visualElements =>
+              visualElements
+                .hcursor
+                .downField("visualElement")
+                .withFocus(visualElement =>
+                  visualElement
+                    .asString
+                    .map(_.replace("data-size=\"fullbredde\"", "data-size=\"full\""))
+                    .map(io.circe.Json.fromString)
+                    .getOrElse(visualElement)
+                )
+                .top
+                .getOrElse(visualElements)
+            }
           )
-          updated.focus.map(f => doc.hcursor.downField("visualElement").withFocus(_ => f).top.getOrElse(doc).noSpaces)
-        } else Some(doc.noSpaces)
-      }
-      .getOrElse(value)
+        )
+        updated
+          .focus
+          .map(f => doc.hcursor.downField("visualElement").withFocus(_ => f).top.getOrElse(doc).noSpaces)
+          .getOrElse(value)
+      } else doc.noSpaces
+    }.getOrElse(value)
   }
 }
