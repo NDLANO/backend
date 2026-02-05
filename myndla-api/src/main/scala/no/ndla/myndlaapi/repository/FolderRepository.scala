16afcb07261cf2db11b3f5c9981208036aadb8f3
Gunnar Velle <gunnar.velle@gmail.com>
2026-02-05 11:58:34 +0100

    Makes get recents work again

--- myndla-api/src/main/scala/no/ndla/myndlaapi/repository/FolderRepository.scala
+++ myndla-api/src/main/scala/no/ndla/myndlaapi/repository/FolderRepository.scala
@@ -319,8 +319,10 @@ class FolderRepository(using clock: Clock, dbUtility: DBUtility) extends StrictL
             limit ${size.getOrElse(1)}
            """
       .one(Resource.fromResultSet(r, withConnection = false))
-      .toOne(rs => ResourceConnection.fromResultSet(fr)(rs).toOption)
-      .map((resource, connection) => resource.map(_.copy(connection = connection)))
+      .toMany(rs => ResourceConnection.fromResultSet(fr)(rs).toOption)
+      .map((resource, connections) =>
+        resource.map(r => r.copy(connection = connections.find(c => c.resourceId == r.id)))
+      )
       .runListFlat()
   }
 
