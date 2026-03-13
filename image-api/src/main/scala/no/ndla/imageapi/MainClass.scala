9a19300de0a718c9d7fecc7b40b7ba85f58c4c8c
Amandus Søve Thorsrud <amandus.thorsrud@knowit.no>
2026-03-13 12:32:31 +0100

    feat: rewrite temporary variant generation to a permanent standalone job

--- image-api/src/main/scala/no/ndla/imageapi/MainClass.scala
+++ image-api/src/main/scala/no/ndla/imageapi/MainClass.scala
@@ -8,8 +8,11 @@
 
 package no.ndla.imageapi
 
+import no.ndla.common.Environment.booleanPropOrFalse
 import no.ndla.common.Warmup
 import no.ndla.network.tapir.NdlaTapirMain
+import no.ndla.imageapi.service.StandaloneVariantGeneration
+import sttp.tapir.server.netty.sync.NettySyncServerBinding
 
 class MainClass(override val props: ImageApiProperties) extends NdlaTapirMain[ComponentRegistry] {
   val componentRegistry = new ComponentRegistry(props)
@@ -28,7 +31,24 @@ class MainClass(override val props: ImageApiProperties) extends NdlaTapirMain[Co
 
   override def beforeStart(): Unit = {
     componentRegistry.migrator.migrate()
-    componentRegistry.imageSearchService.createEmptyIndexIfNoIndexesExist()
-    componentRegistry.tagSearchService.createEmptyIndexIfNoIndexesExist()
+
+    if (!booleanPropOrFalse("STANDALONE_VARIANT_GENERATION_ENABLED")) {
+      componentRegistry.imageSearchService.createEmptyIndexIfNoIndexesExist()
+      componentRegistry.tagSearchService.createEmptyIndexIfNoIndexesExist()
+    }
+  }
+
+  override def startServerAndWait(name: String, port: Int)(onStartup: NettySyncServerBinding => Unit): Unit = {
+    if (booleanPropOrFalse("STANDALONE_VARIANT_GENERATION_ENABLED")) {
+      StandaloneVariantGeneration(
+        componentRegistry.imageConverter,
+        componentRegistry.writeService,
+        componentRegistry.imageStorage,
+        componentRegistry.imageRepository,
+        componentRegistry.dbUtility,
+      ).doStandaloneVariantGeneration()
+    } else {
+      super.startServerAndWait(name, port)(onStartup)
+    }
   }
 }
