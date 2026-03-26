565a3db45f17854b40846816b19eafd5deffdf51
Jonas Natten <jonas.natten@knowit.no>
2026-03-26 10:37:11 +0100

    wip

---
+++ network/src/main/scala/no/ndla/network/clients/matomo/MatomoApiClient.scala
@@ -0,0 +1,7 @@
+package no.ndla.network.clients.matomo
+
+import no.ndla.network.NdlaClient
+
+class MatomoApiClient(using props: MatomoProps, client: NdlaClient) {
+  
+}
---
+++ network/src/main/scala/no/ndla/network/clients/matomo/MatomoProps.scala
@@ -0,0 +1,8 @@
+package no.ndla.network.clients.matomo
+
+import no.ndla.common.configuration.{BaseProps, Prop}
+
+trait MatomoProps extends BaseProps {
+  val MatomoUrl: Prop[String]    = prop("MATOMO_URL")
+  val MatomoSiteId: Prop[String] = prop("MATOMO_SITE_ID")
+}
