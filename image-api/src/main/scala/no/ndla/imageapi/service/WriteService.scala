8a321267539be85ed4b81bdacdcca57fc59938a6
Gunnar Velle <gunnar.velle@gmail.com>
2026-03-27 14:55:14 +0100

    Flatten results

--- image-api/src/main/scala/no/ndla/imageapi/service/WriteService.scala
+++ image-api/src/main/scala/no/ndla/imageapi/service/WriteService.scala
@@ -672,7 +672,7 @@ class WriteService(using
                 }
             }
 
-            Try(Await.result(storeResultsFuture, 5.minutes))
+            Try(Await.result(storeResultsFuture, 5.minutes)).flatten
           }
           .collectFirst { case Failure(ex) =>
             ex
