a487c43a7315c62ed9c7ac6c6f19da2e7cb11c6a
Gunnar Velle <gunnar.velle@gmail.com>
2026-04-10 10:22:50 +0200

    Drop boolean on enum. Filter more explicitly

--- search-api/src/main/scala/no/ndla/searchapi/model/domain/DraftSearchField.scala
+++ search-api/src/main/scala/no/ndla/searchapi/model/domain/DraftSearchField.scala
@@ -13,25 +13,25 @@ import sttp.tapir.Codec.PlainCodec
 import sttp.tapir.Schema
 import sttp.tapir.codec.enumeratum.*
 
-sealed abstract class DraftSearchField(override val entryName: String, val alwaysInclude: Boolean) extends EnumEntry
+sealed abstract class DraftSearchField(override val entryName: String) extends EnumEntry
 
 object DraftSearchField extends Enum[DraftSearchField] with CirceEnum[DraftSearchField] {
 
   val values: IndexedSeq[DraftSearchField] = findValues
 
-  case object Title           extends DraftSearchField("title", true)
-  case object Introduction    extends DraftSearchField("introduction", true)
-  case object MetaDescription extends DraftSearchField("metaDescription", true)
-  case object Disclaimer      extends DraftSearchField("disclaimer", true)
-  case object Content         extends DraftSearchField("content", true)
-  case object Tags            extends DraftSearchField("tags", true)
-  case object EmbedAttributes extends DraftSearchField("embedAttributes", true)
-  case object Creators        extends DraftSearchField("creators", true)
-  case object Processors      extends DraftSearchField("processors", true)
-  case object Rightsholders   extends DraftSearchField("rightsholders", true)
-  case object RevisionMeta    extends DraftSearchField("revisionMeta", true)
-  case object Notes           extends DraftSearchField("notes", false)
-  case object PreviousNotes   extends DraftSearchField("previousNotes", false)
+  case object Title           extends DraftSearchField("title")
+  case object Introduction    extends DraftSearchField("introduction")
+  case object MetaDescription extends DraftSearchField("metaDescription")
+  case object Disclaimer      extends DraftSearchField("disclaimer")
+  case object Content         extends DraftSearchField("content")
+  case object Tags            extends DraftSearchField("tags")
+  case object EmbedAttributes extends DraftSearchField("embedAttributes")
+  case object Creators        extends DraftSearchField("creators")
+  case object Processors      extends DraftSearchField("processors")
+  case object Rightsholders   extends DraftSearchField("rightsholders")
+  case object RevisionMeta    extends DraftSearchField("revisionMeta")
+  case object Notes           extends DraftSearchField("notes")
+  case object PreviousNotes   extends DraftSearchField("previousNotes")
 
   implicit val schema: Schema[DraftSearchField]    = schemaForEnumEntry[DraftSearchField]
   implicit val codec: PlainCodec[DraftSearchField] = plainCodecEnumEntry[DraftSearchField]
