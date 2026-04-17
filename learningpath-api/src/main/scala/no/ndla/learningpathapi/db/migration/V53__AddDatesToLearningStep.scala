9b24a961dac2fd1a30d58965f56208332fab387a
Jonas Carlsen <jonas.carlsen@knowit.no>
2026-04-17 11:45:39 +0200

    chore: drop LearningpathAndStepMigration

--- learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V53__AddDatesToLearningStep.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V53__AddDatesToLearningStep.scala
@@ -8,28 +8,6 @@
 
 package no.ndla.learningpathapi.db.migration
 
-import io.circe.Json
-import no.ndla.common.CirceUtil
-import no.ndla.learningpathapi.db.util.*
+import no.ndla.database.FinishedMigration
 
-class V53__AddDatesToLearningStep extends LearningPathAndStepMigration {
-  override def convertPathAndSteps(
-      lpData: LpDocumentRow,
-      stepDatas: List[StepDocumentRow],
-  ): (LpDocumentRow, List[StepDocumentRow]) = {
-    val learningpath = CirceUtil.unsafeParse(lpData.learningPathDocument)
-    val created      = learningpath.hcursor.get[String]("created") match {
-      case Left(value)  => throw new RuntimeException("Parsing learningpath failed")
-      case Right(value) => value
-    }
-    val updatedSteps = stepDatas.map { step =>
-      val oldDocument = CirceUtil.unsafeParse(step.learningStepDocument)
-      val newDocument = oldDocument
-        .mapObject(_.add("created", Json.fromString(created)).add("lastUpdated", Json.fromString(created)))
-        .noSpaces
-      step.copy(learningStepDocument = newDocument)
-    }
-
-    (lpData, updatedSteps)
-  }
-}
+class V53__AddDatesToLearningStep extends FinishedMigration
--- learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V57__AddOwnerToLearningStep.scala
+++ learningpath-api/src/main/scala/no/ndla/learningpathapi/db/migration/V57__AddOwnerToLearningStep.scala
@@ -8,26 +8,6 @@
 
 package no.ndla.learningpathapi.db.migration
 
-import io.circe.Json
-import no.ndla.common.CirceUtil
-import no.ndla.learningpathapi.db.util.*
+import no.ndla.database.FinishedMigration
 
-class V57__AddOwnerToLearningStep extends LearningPathAndStepMigration {
-  override def convertPathAndSteps(
-      lpData: LpDocumentRow,
-      stepDatas: List[StepDocumentRow],
-  ): (LpDocumentRow, List[StepDocumentRow]) = {
-    val learningpath = CirceUtil.unsafeParse(lpData.learningPathDocument)
-    val owner        = learningpath.hcursor.get[String]("owner") match {
-      case Left(value)  => throw new RuntimeException("Parsing learningpath failed")
-      case Right(value) => value
-    }
-    val updatedSteps = stepDatas.map { step =>
-      val oldDocument = CirceUtil.unsafeParse(step.learningStepDocument)
-      val newDocument = oldDocument.mapObject(_.add("owner", Json.fromString(owner))).noSpaces
-      step.copy(learningStepDocument = newDocument)
-    }
-
-    (lpData, updatedSteps)
-  }
-}
+class V57__AddOwnerToLearningStep extends FinishedMigration
