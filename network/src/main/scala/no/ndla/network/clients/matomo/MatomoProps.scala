88c58bc87c269f85092a4136cc99b49f065aad6f
Jonas Natten <jonas.natten@knowit.no>
2026-05-07 08:55:45 +0200

    refactor: change matomo token variable

--- network/src/main/scala/no/ndla/network/clients/matomo/MatomoProps.scala
+++ network/src/main/scala/no/ndla/network/clients/matomo/MatomoProps.scala
@@ -13,5 +13,5 @@ import no.ndla.common.configuration.{BaseProps, Prop}
 trait MatomoProps extends BaseProps {
   val MatomoUrl: Prop[String]       = prop("MATOMO_URL")
   val MatomoSiteId: Prop[String]    = prop("MATOMO_SITE_ID")
-  val MatomoTokenAuth: Prop[String] = prop("MATOMO_TOKEN_AUTH")
+  val MatomoTokenAuth: Prop[String] = prop("MATOMO_API_TOKEN")
 }
